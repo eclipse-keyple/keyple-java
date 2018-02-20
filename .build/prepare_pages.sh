c6717d445cae19c824c3c305a5639137f68983d9
Florent Clairambault <florent@clairambault.fr>
2018-02-20 22:59:09 +0100

    Conditional handling

--- .build/prepare_pages.sh
+++ .build/prepare_pages.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mkdir -p pages/javadoc pages/tests
 mv keyple-core/build/docs/javadoc pages/javadoc/core
-echo "<html><body>K E Y P L E</body></html>" >pages/index.html
+echo "<html><body>K E Y P L E - $TRAVIS_BRANCH</body></html>" >pages/index.html
