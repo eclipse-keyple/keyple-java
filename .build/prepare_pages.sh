de7c90e194db99591ff33cf5f371b68f1716b488
Florent Clairambault <florent@clairambault.fr>
2018-02-20 23:15:02 +0100

    An other test

--- .build/prepare_pages.sh
+++ .build/prepare_pages.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-mkdir -p pages/javadoc pages/tests
-mv keyple-core/build/docs/javadoc pages/javadoc/core
+mkdir -p pages/$TRAVIS_BRANCH/javadoc
+mv keyple-core/build/docs/javadoc pages/$TRAVIS_BRANCH/javadoc/core
 echo "<html><body>K E Y P L E</body></html>" >pages/index.html
-
-if [ $TRAVIS_BRANCH = "master" ] || [ $TRAVIS_BRANCH = "feature-publish-javadoc" ]; then
-    export DEPLOY=true
-fi
