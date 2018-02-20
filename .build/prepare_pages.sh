66f3b8a810eb573c5c09e8c0c31556fd4dd3f1b3
Florent Clairambault <florent@clairambault.fr>
2018-02-20 23:27:24 +0100

    An other attempt at using the condition

--- .build/prepare_pages.sh
+++ .build/prepare_pages.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
-mkdir -p pages/$TRAVIS_BRANCH/javadoc
-mv keyple-core/build/docs/javadoc pages/$TRAVIS_BRANCH/javadoc/core
+#!/bin/sh -x
+mkdir -p pages/${TRAVIS_BRANCH}/javadoc
+mv keyple-core/build/docs/javadoc pages/${TRAVIS_BRANCH}/javadoc/core
 echo "<html><body>K E Y P L E</body></html>" >pages/index.html
