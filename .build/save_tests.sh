7ac70af30cf08d8b3750a64f6ec53ab644b53735
Florent Clairambault <florent@clairambault.fr>
2018-02-23 17:16:50 +0100

    other tests

--- .build/save_tests.sh
+++ .build/save_tests.sh
@@ -3,11 +3,12 @@ mkdir -p ~/tests ~/artifacts
 # find . -path "*/" -exec cp {} ~/junit \;
 for dir in keyple-*
 do
-    if [ ! -d $dir/build/test-results/test ]; then
-       continue
+    if [ -d $dir/build/test-results/test ]; then
+       mkdir -p ~/tests/$dir
+       mv $dir/build/test-results/test/*.xml ~/tests/$dir
     fi
-    mv $dir/build/test-results/test/*.xml ~/tests
-    mkdir -p ~/artifacts/tests/$dir
-    mv $dir/build/reports/tests/test ~/artifacts/tests/$dir
+    if [ -d $dir/build/reports/tests/test ]; then
+       mv $dir/build/reports/tests/test ~/artifacts/tests/$dir
+    fi
 done
 
