8a5fba565a4217342830b4dcdf4b7864176a6175
Florent Clairambault <florent@clairambault.fr>
2018-02-23 17:56:43 +0100

    other tests

--- .circleci/save_artifacts.sh
+++ .circleci/save_artifacts.sh
@@ -7,24 +7,29 @@ for dir in keyple-*
 do
     # Junit tests --> ~/tests
     if [ -d $dir/build/test-results/test ]; then
-       mkdir -p ~/tests/$dir
-       mv $dir/build/test-results/test/*.xml ~/tests/$dir
+        mkdir -p ~/tests/$dir
+        cp -a $dir/build/test-results/test/*.xml ~/tests/$dir
     fi
 
     # Junit HTML view --> ~artifacts/$project/tests
     if [ -d $dir/build/reports/tests/test ]; then
-       mkdir -p ~/artifacts/$dir
-       mv $dir/build/reports/tests/test ~/artifacts/$dir/tests
+        mkdir -p ~/artifacts/$dir
+        cp -a $dir/build/reports/tests/test ~/artifacts/$dir/tests
+
+        mkdir -p ~/pages/$dir
+        cp -a $dir/build/reports/tests/test ~/pages/$dir/tests
     fi
 
     # Javadoc --> ~/artifacts/$project/javadoc
     if [ -d $dir/build/docs/javadoc ]; then
-        mv $dir/build/docs/javadoc ~/artifacts/$dir/javadoc
+        cp -a $dir/build/docs/javadoc ~/artifacts/$dir/javadoc
+
+        mkdir -p ~/pages/$dir
+        cp -a $dir/build/docs/javadoc ~/pages/$dir/javadoc
     fi
 
     # JMH --> ~/artifacts/$project/jmh
     if [ -f $dir/build/reports/jmh/results.txt ]; then
-       mv $dir/build/reports/jmh/results.txt ~/artifacts/$dir/jmh.txt
+        mv $dir/build/reports/jmh/results.txt ~/artifacts/$dir/jmh.txt
     fi
 done
-
