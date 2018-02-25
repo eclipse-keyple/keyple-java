625e1d073ae7972630bf0b7069a495a45e03dd5e
Florent Clairambault <florent@clairambault.fr>
2018-02-25 20:04:19 +0100

    Turns out it was already converted

--- keyple-calypso/src/main/java/org/keyple/calypso/commands/utils/ResponseUtils.java
+++ keyple-calypso/src/main/java/org/keyple/calypso/commands/utils/ResponseUtils.java
@@ -8,9 +8,7 @@
 
 package org.keyple.calypso.commands.utils;
 
-import java.nio.ByteBuffer;
-import org.keyple.calypso.commands.po.parser.GetDataFciRespPars;
-import org.keyple.seproxy.ByteBufferUtils;
+
 
 /**
  * This class eases the parse of APDUResponses into objects.
@@ -24,71 +22,6 @@ public class ResponseUtils {
      */
     private ResponseUtils() {
 
-    }
-
-    /**
-     * Method to get the FCI from the response.
-     *
-     * @param apduResponse the apdu response
-     * @return the FCI template
-     */
-    public static GetDataFciRespPars.FCI toFCI(ByteBuffer apduResponse) {
-        GetDataFciRespPars.StartupInformation startupInformation = null;
-        byte firstResponseApdubyte = apduResponse.get(0);
-        ByteBuffer dfName = null;
-        ByteBuffer fciProprietaryTemplate = null;
-        ByteBuffer fciIssuerDiscretionaryData = null;
-        ByteBuffer applicationSN = null;
-        ByteBuffer discretionaryData;
-
-        if ((byte) 0x6F == firstResponseApdubyte) {
-            int aidLength = apduResponse.get(3);
-            int fciTemplateLength = apduResponse.get(5 + aidLength);
-            int fixedPartOfFciTemplate = fciTemplateLength - 22;
-            int firstbyteAid = 6 + aidLength + fixedPartOfFciTemplate;
-            int fciIssuerDiscretionaryDataLength =
-                    apduResponse.get(8 + aidLength + fixedPartOfFciTemplate);
-            int firstbyteFciIssuerDiscretionaryData = 9 + aidLength + fixedPartOfFciTemplate;
-            int applicationSNLength = apduResponse.get(10 + aidLength + fixedPartOfFciTemplate);
-            int firstbyteApplicationSN = 11 + aidLength + fixedPartOfFciTemplate;
-            int discretionaryDataLength = apduResponse.get(20 + aidLength + fixedPartOfFciTemplate);
-            int firstbyteDiscretionaryData = 21 + aidLength + fixedPartOfFciTemplate;
-
-            if ((byte) 0x84 == apduResponse.get(2)) {
-                dfName = ByteBufferUtils.subIndex(apduResponse, 4, 4 + aidLength);
-            }
-
-            if ((byte) 0xA5 == apduResponse.get(4 + aidLength)) {
-                fciProprietaryTemplate = ByteBufferUtils.subIndex(apduResponse, firstbyteAid,
-                        firstbyteAid + fciTemplateLength);
-            }
-
-            if ((byte) 0xBF == apduResponse.get(6 + aidLength + fixedPartOfFciTemplate)
-                    && ((byte) 0x0C == apduResponse.get(7 + aidLength + fixedPartOfFciTemplate))) {
-                fciIssuerDiscretionaryData = ByteBufferUtils.subIndex(apduResponse,
-                        firstbyteFciIssuerDiscretionaryData,
-                        firstbyteFciIssuerDiscretionaryData + fciIssuerDiscretionaryDataLength);
-            }
-
-            if ((byte) 0xC7 == apduResponse.get(9 + aidLength + fixedPartOfFciTemplate)) {
-                applicationSN = ByteBufferUtils.subIndex(apduResponse, firstbyteApplicationSN,
-                        firstbyteApplicationSN + applicationSNLength);
-            }
-
-            if ((byte) 0x53 == apduResponse.get(19 + aidLength + fixedPartOfFciTemplate)) {
-                discretionaryData =
-                        ByteBufferUtils.subIndex(apduResponse, firstbyteDiscretionaryData,
-                                firstbyteDiscretionaryData + discretionaryDataLength);
-                startupInformation =
-                        new GetDataFciRespPars.StartupInformation(discretionaryData.get(0),
-                                discretionaryData.get(1), discretionaryData.get(2),
-                                discretionaryData.get(3), discretionaryData.get(4),
-                                discretionaryData.get(5), discretionaryData.get(6));
-            }
-        }
-
-        return new GetDataFciRespPars.FCI(dfName, fciProprietaryTemplate,
-                fciIssuerDiscretionaryData, applicationSN, startupInformation);
     }
 
     /**
