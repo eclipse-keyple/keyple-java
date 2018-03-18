f8eadd5c08fe835b397b72337185c1c449a50572
Florent Clairambault <florent@clairambault.fr>
2018-03-18 20:58:04 +0100

    save_artifacts: Adding some logs

--- .circleci/save_artifacts.sh
+++ .circleci/save_artifacts.sh
@@ -5,14 +5,18 @@ find . -name "keyple-*.jar" -not -name "*-jmh.jar" -exec cp {} ~/artifacts/jars 
 
 for dir in keyple-*
 do
+    echo "* Dir $dir:"
+
     # Junit tests --> ~/tests
     if [ -d $dir/build/test-results/test ]; then
+        echo "   * Found junit tests"
         mkdir -p ~/tests/$dir
         cp -a $dir/build/test-results/test/*.xml ~/tests/$dir
     fi
 
     # Junit HTML view --> ~artifacts/$project/tests
     if [ -d $dir/build/reports/tests/test ]; then
+        echo "   * Found html view of tests"
         mkdir -p ~/artifacts/$dir
         cp -a $dir/build/reports/tests/test ~/artifacts/$dir/tests
 
@@ -22,6 +26,7 @@ do
 
     # Javadoc --> ~/artifacts/$project/javadoc
     if [ -d $dir/build/docs/javadoc ]; then
+        echo "   * Found javadoc"
         mkdir -p ~/artifacts/$dir
         cp -a $dir/build/docs/javadoc ~/artifacts/$dir/javadoc
 
@@ -31,6 +36,7 @@ do
 
     # JMH --> ~/artifacts/$project/jmh
     if [ -f $dir/build/reports/jmh/results.txt ]; then
+        echo "   * Found JMH"
         mv $dir/build/reports/jmh/results.txt ~/artifacts/$dir/jmh.txt
     fi
 done
