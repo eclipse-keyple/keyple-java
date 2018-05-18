4edf173905f6d0e59840e7018fd872689ba06f9f
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2018-05-18 22:59:00 +0200

    Optimize imports to fix PMD rules violations

--- keyple-example-common/src/main/java/org/keyple/example/common/KeepOpenCardTimeoutManager.java
+++ keyple-example-common/src/main/java/org/keyple/example/common/KeepOpenCardTimeoutManager.java
@@ -8,14 +8,14 @@
 
 package org.keyple.example.common;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import org.keyple.calypso.commands.po.PoRevision;
 import org.keyple.calypso.commands.po.builder.ReadRecordsCmdBuild;
 import org.keyple.calypso.commands.po.builder.UpdateRecordCmdBuild;
 import org.keyple.seproxy.*;
-import org.keyple.seproxy.SeRequestSet;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Set of @{@link SeRequestSet} to test NFC Plugin with keep Open Channel. Two sets
