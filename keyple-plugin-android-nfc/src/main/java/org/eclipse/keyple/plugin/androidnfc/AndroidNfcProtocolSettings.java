ca482f071079eaab26d305a68fc650d43f4630a6
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2018-06-04 12:33:43 +0200

    Added missing file

---
+++ keyple-plugin-android-nfc/src/main/java/org/eclipse/keyple/plugin/androidnfc/AndroidNfcProtocolSettings.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2018 Calypso Networks Association https://www.calypsonet-asso.org/
+ *
+ * All rights reserved. This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License version 2.0 which accompanies this distribution, and is
+ * available at https://www.eclipse.org/org/documents/epl-2.0/EPL-2.0.html
+ */
+
+package org.eclipse.keyple.plugin.androidnfc;
+
+public interface AndroidNfcProtocolSettings {
+
+    public static String TAG_TECHNOLOGY_ISO14443_4 = "android.nfc.tech.IsoDep";
+
+    public static String TAG_TECHNOLOGY_MIFARE_UL = "android.nfc.tech.MifareUltralight";
+
+    public static String TAG_TECHNOLOGY_MIFARE_CLASSIC = "android.nfc.tech.MifareClassic";
+}
