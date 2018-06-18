385335d8927762dc2afac871d379f5b6ca6955af
OlivierDelcroix <odelcroi@gmail.com>
2018-06-18 16:22:09 +0200

    Merge remote-tracking branch 'origin/develop' into nfc_refactor
    # Conflicts:
    #	keyple-core/src/main/java/org/eclipse/keyple/seproxy/event/AbstractObservableReader.java
    #	keyple-example/android/nfc/src/main/java/org/eclipse/keyple/examples/androidnfc/NFCTestFragment.java

--- keyple-plugins/android-nfc/src/main/java/org/eclipse/keyple/plugin/androidnfc/AndroidNfcReader.java
+++ keyple-plugins/android-nfc/src/main/java/org/eclipse/keyple/plugin/androidnfc/AndroidNfcReader.java
@@ -21,7 +21,7 @@ import org.eclipse.keyple.seproxy.event.ReaderEvent;
 import org.eclipse.keyple.seproxy.exception.ChannelStateReaderException;
 import org.eclipse.keyple.seproxy.exception.IOReaderException;
 import org.eclipse.keyple.seproxy.exception.InvalidMessageException;
-import org.eclipse.keyple.seproxy.plugin.AbstractLocalReader;
+import org.eclipse.keyple.seproxy.local.AbstractLocalReader;
 import org.eclipse.keyple.seproxy.protocol.ContactlessProtocols;
 import org.eclipse.keyple.seproxy.protocol.SeProtocolSettings;
 import org.eclipse.keyple.util.ByteBufferUtils;
