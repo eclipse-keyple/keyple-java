7ff4d4507f75fead74bbacaff6dff30b460c4b51
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2018-07-06 19:54:03 +0200

    Added missing files

---
+++ keyple-core/src/main/java/org/eclipse/keyple/seproxy/event/ObservableReader.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2018 Calypso Networks Association https://www.calypsonet-asso.org/
+ *
+ * All rights reserved. This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License version 2.0 which accompanies this distribution, and is
+ * available at https://www.eclipse.org/org/documents/epl-2.0/EPL-2.0.html
+ */
+
+package org.eclipse.keyple.seproxy.event;
+
+import org.eclipse.keyple.seproxy.ProxyReader;
+import org.eclipse.keyple.util.Observable;
+
+public interface ObservableReader extends ProxyReader {
+    interface ReaderObserver extends Observable.Observer<ReaderEvent> {
+    }
+
+    void addObserver(ReaderObserver observer);
+
+    void removeObserver(ReaderObserver observer);
+
+    void notifyObservers(ReaderEvent event);
+}
