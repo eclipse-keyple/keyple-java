26f89a74aa409ce889b0f3639790702e81d50f51
olivier d <odelcroi@gmail.com>
2018-07-12 16:31:05 +0200

    echo ls

--- .circleci/save_artifacts_android.sh
+++ .circleci/save_artifacts_android.sh
@@ -2,10 +2,10 @@
 
 . .circleci/functions.sh
 
-mkdir -p ~/artifacts/jars ~/pages/jars
-find . -name "keyple-*.jar" -not -name "*-jmh.jar" \
-  -exec cp {} ~/artifacts/jars \; \
-  -exec cp {} ~/pages/jars \;
+mkdir -p ~/artifacts/aar ~/pages/aar
+find . -name "keyple-*.aar"  \
+  -exec cp {} ~/artifacts/aar \; \
+  -exec cp {} ~/pages/aar \;
 
 
 save_directory keyple-example/android/nfc
@@ -13,8 +13,8 @@ save_directory keyple-example/android/omapi
 save_directory keyple-plugin/android-nfc
 save_directory keyple-plugin/android-omapi
 
-cp .build/web/* ~/pages/
-
-
+echo "End of save artifacts android"
+ls ~/pages/
+ls ~/artifacts/
 
 
