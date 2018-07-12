9dbfc344da4eccd96996bef9110a2b7a8b4a660b
olivier d <odelcroi@gmail.com>
2018-07-12 11:50:16 +0200

    rename androidnfc package to android.nfc

--- keyple-plugin/android-nfc/src/test/java/org/eclipse/keyple/plugin/android/nfc/TagProxyTest.java
+++ keyple-plugin/android-nfc/src/test/java/org/eclipse/keyple/plugin/android/nfc/TagProxyTest.java
@@ -6,7 +6,7 @@
  * available at https://www.eclipse.org/org/documents/epl-2.0/EPL-2.0.html
  */
 
-package org.eclipse.keyple.plugin.androidnfc;
+package org.eclipse.keyple.plugin.android.nfc;
 
 import static junit.framework.Assert.assertEquals;
 import static org.mockito.Mockito.when;
