75639cbf9ed7f4a80b75ebdb9f897e0559340b29
olivier d <odelcroi@gmail.com>
2018-07-13 14:48:43 +0200

    fix paths

--- .circleci/save_artifacts_android.sh
+++ .circleci/save_artifacts_android.sh
@@ -5,10 +5,10 @@
 #save artifacts of keyple core projects (java)
 /bin/bash .circleci/save_artifacts.sh
 
-mkdir -p ~/artifacts/jar ~/pages/jar
+mkdir -p ~/artifacts/jars ~/pages/jars
 find . -name "*.aar"  \
-  -exec cp {} ~/artifacts/jar \; \
-  -exec cp {} ~/pages/jar \;
+  -exec cp {} ~/artifacts/jars \; \
+  -exec cp {} ~/pages/jars \;
 
 save_directory keyple-example/android/nfc
 save_directory keyple-example/android/omapi
