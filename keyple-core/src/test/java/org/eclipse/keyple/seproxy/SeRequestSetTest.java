ff0262b6bba6862693195a9ff863466ee18aab31
olivier d <odelcroi@gmail.com>
2018-07-17 17:02:27 +0200

    add unit test SeRequest

---
+++ keyple-core/src/test/java/org/eclipse/keyple/seproxy/SeRequestSetTest.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2018 Calypso Networks Association https://www.calypsonet-asso.org/
+ *
+ * All rights reserved. This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License version 2.0 which accompanies this distribution, and is
+ * available at https://www.eclipse.org/org/documents/epl-2.0/EPL-2.0.html
+ */
+
+package org.eclipse.keyple.seproxy;
+
+import static org.junit.Assert.*;
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SeRequestSetTest {
+
+    // object to test
+    SeRequestSet seRequestSet;
+
+    // attributes
+    Set<SeRequest> sortedRequests;
+
+    SeRequest firstRequest;
+
+
+    @Before
+    public void setUp() throws Exception {
+        // init
+        sortedRequests = new HashSet<SeRequest>();
+
+        firstRequest = SeRequestTest.getSeRequestSample();
+        sortedRequests.add(firstRequest);
+
+        // init
+        seRequestSet = new SeRequestSet(sortedRequests);
+    }
+
+    @Test
+    public void getRequests() {
+        Assert.assertArrayEquals(sortedRequests.toArray(), seRequestSet.getRequests().toArray());
+    }
+
+    @Test
+    public void getSingleRequest() {
+        seRequestSet = new SeRequestSet(firstRequest);
+
+        // with only one element it works
+        Assert.assertEquals(firstRequest, seRequestSet.getSingleRequest());
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void getSingleRequestFail() {
+        // put a second element
+        sortedRequests.add(SeRequestTest.getSeRequestSample());
+        seRequestSet = new SeRequestSet(sortedRequests);
+        seRequestSet.getSingleRequest();// raise exception because it works only with one element
+    }
+
+    @Test
+    public void toStringNull() {
+        seRequestSet = new SeRequestSet(new HashSet<SeRequest>());
+        Assert.assertNotNull(seRequestSet.toString());
+    }
+
+    /*
+     * CONSTRUCTOR
+     */
+
+
+}
--- keyple-core/src/test/java/org/eclipse/keyple/seproxy/SeRequestTest.java
+++ keyple-core/src/test/java/org/eclipse/keyple/seproxy/SeRequestTest.java
@@ -11,39 +11,176 @@ package org.eclipse.keyple.seproxy;
 import static org.junit.Assert.*;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.eclipse.keyple.seproxy.protocol.ContactlessProtocols;
+import org.eclipse.keyple.util.ByteBufferUtils;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class SeRequestTest {
 
     private static final ByteBuffer aid = ByteBuffer.wrap(new byte[] {(byte) 0x01, (byte) 0x02});
 
+    // object to test
+    SeRequest seRequest;
+
+    public List<ApduRequest> getApdus() {
+        return apdus;
+    }
+
+    // attributes
+    List<ApduRequest> apdus;
+    Boolean keepChannelOpen;
+    SeProtocol seProtocol;
+    Set<Short> selectionStatusCode;
+
+
+
+    @Before
+    public void setUp() throws Exception {
+
+        ByteBuffer bytes = ByteBufferUtils.fromHex("01");
+        apdus = new ArrayList<ApduRequest>();
+        apdus.add(new ApduRequest(bytes, true));
+        apdus.add(new ApduRequest(bytes, false));
+
+        keepChannelOpen = true;
+
+        seProtocol = ContactlessProtocols.PROTOCOL_B_PRIME;
+
+        selectionStatusCode = new HashSet<Short>();
+        selectionStatusCode.add(new Short("1"));
+
+        seRequest = new SeRequest(aid, apdus, keepChannelOpen, seProtocol, selectionStatusCode);
+    }
+
     @Test
     public void testSERequest() {
-        SeRequestSet request =
-                new SeRequestSet(new SeRequest(aid, new ArrayList<ApduRequest>(), true));
-        assertNotNull(request);
+        assertNotNull(seRequest);
+    }
+
+
+    @Test
+    public void getAidToSelect() {
+        // test
+        Assert.assertEquals(aid, seRequest.getAidToSelect());
+
     }
 
     @Test
-    public void testGetAidToSelect() {
-        SeRequestSet request =
-                new SeRequestSet(new SeRequest(aid, new ArrayList<ApduRequest>(), true));
-        assertEquals(aid, request.getSingleRequest().getAidToSelect());
+    public void getApduRequests() {
+        // test
+        seRequest = new SeRequest(aid, apdus, false);
+        Assert.assertArrayEquals(apdus.toArray(), seRequest.getApduRequests().toArray());
     }
 
     @Test
-    public void testGetApduRequests() {
-        SeRequestSet request =
-                new SeRequestSet(new SeRequest(aid, new ArrayList<ApduRequest>(), true));
-        assertArrayEquals(new ArrayList<ApduRequest>().toArray(),
-                request.getSingleRequest().getApduRequests().toArray());
+    public void isKeepChannelOpen() {
+        assertTrue(seRequest.isKeepChannelOpen());
     }
 
     @Test
-    public void testAskKeepChannelOpen() {
-        SeRequestSet request =
-                new SeRequestSet(new SeRequest(aid, new ArrayList<ApduRequest>(), true));
-        assertTrue(request.getSingleRequest().isKeepChannelOpen());
+    public void getProtocolFlag() {
+        seRequest = new SeRequest(aid, new ArrayList<ApduRequest>(), true, seProtocol);
+        Assert.assertEquals(seProtocol, seRequest.getProtocolFlag());
     }
+
+    @Test
+    public void getSuccessfulSelectionStatusCodes() {
+        seRequest = new SeRequest(aid, new ArrayList<ApduRequest>(), true,
+                ContactlessProtocols.PROTOCOL_B_PRIME, selectionStatusCode);
+        Assert.assertArrayEquals(selectionStatusCode.toArray(),
+                seRequest.getSuccessfulSelectionStatusCodes().toArray());
+    }
+
+    @Test
+    public void toStringNull() {
+        seRequest = new SeRequest(null, null, true, null, null);
+        Assert.assertNotNull(seRequest.toString());
+    }
+
+    /*
+     * Constructors
+     */
+    @Test
+    public void constructor1() {
+        seRequest = new SeRequest(aid, apdus, keepChannelOpen);
+        Assert.assertEquals(aid, seRequest.getAidToSelect());
+        Assert.assertEquals(keepChannelOpen, seRequest.isKeepChannelOpen());
+        Assert.assertArrayEquals(apdus.toArray(), seRequest.getApduRequests().toArray());
+        //
+        Assert.assertNull(seRequest.getProtocolFlag());
+        Assert.assertNull(seRequest.getSuccessfulSelectionStatusCodes());
+    }
+
+    @Test
+    public void constructor2() {
+        seRequest = new SeRequest(aid, apdus, keepChannelOpen, seProtocol);
+        Assert.assertEquals(aid, seRequest.getAidToSelect());
+        Assert.assertEquals(keepChannelOpen, seRequest.isKeepChannelOpen());
+        Assert.assertArrayEquals(apdus.toArray(), seRequest.getApduRequests().toArray());
+        Assert.assertEquals(seProtocol, seRequest.getProtocolFlag());
+        //
+        Assert.assertNull(seRequest.getSuccessfulSelectionStatusCodes());
+    }
+
+    @Test
+    public void constructor2b() {
+        seRequest = new SeRequest(aid, apdus, keepChannelOpen, selectionStatusCode);
+        Assert.assertEquals(aid, seRequest.getAidToSelect());
+        Assert.assertEquals(keepChannelOpen, seRequest.isKeepChannelOpen());
+        Assert.assertArrayEquals(apdus.toArray(), seRequest.getApduRequests().toArray());
+        Assert.assertNull(seRequest.getProtocolFlag());
+        //
+        Assert.assertArrayEquals(selectionStatusCode.toArray(),
+                seRequest.getSuccessfulSelectionStatusCodes().toArray());
+    }
+
+    @Test
+    public void constructor3() {
+        seRequest = new SeRequest(aid, apdus, keepChannelOpen, seProtocol, selectionStatusCode);
+        Assert.assertEquals(aid, seRequest.getAidToSelect());
+        Assert.assertEquals(keepChannelOpen, seRequest.isKeepChannelOpen());
+        Assert.assertArrayEquals(apdus.toArray(), seRequest.getApduRequests().toArray());
+        Assert.assertEquals(seProtocol, seRequest.getProtocolFlag());
+        Assert.assertArrayEquals(selectionStatusCode.toArray(),
+                seRequest.getSuccessfulSelectionStatusCodes().toArray());
+    }
+
+
+    /*
+     * HELPERS FOR OTHERS TESTS SUITE
+     */
+
+    static SeRequest getSeRequestSample() {
+        List<ApduRequest> apdus;
+        Boolean keepChannelOpen;
+        SeProtocol seProtocol;
+        Set<Short> selectionStatusCode;
+        ByteBuffer bytes = ByteBufferUtils.fromHex("01");
+
+
+        apdus = new ArrayList<ApduRequest>();
+        apdus.add(new ApduRequest(bytes, true));
+        apdus.add(new ApduRequest(bytes, false));
+
+        keepChannelOpen = true;
+
+        seProtocol = ContactlessProtocols.PROTOCOL_B_PRIME;
+
+        selectionStatusCode = new HashSet<Short>();
+        selectionStatusCode.add(new Short("1"));
+
+        return new SeRequest(aid, apdus, keepChannelOpen, seProtocol, selectionStatusCode);
+
+    }
+
+
 
 }
