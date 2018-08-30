dece0c457d03df46c187ddd12f54d43e3452e8e0
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2018-08-30 20:59:19 +0200

    Removed deprecated PcSc logger classes

--- keyple-plugin/pcsc/src/main/java/org/eclipse/keyple/plugin/pcsc/log/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * PCSC spy/logging package. This allows to wrap a {@link javax.smartcardio.CardTerminals} object
- * and get all the associated classes with the correct logging information.
- */
-package org.eclipse.keyple.plugin.pcsc.log;
