6ebff18d1d673ca537189836a1efa1fed162cc65
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2018-09-11 18:39:07 +0200

    Streamlined Demo code: Added two "launchers" for PcSc and Stub Hoplink demos, Added context agnostic Demo_HoplinkTransactionEngine, Reviewed demo code, Added time measurement with slf4j Profiler.

--- keyple-example/pc/src/main/java/org/eclipse/keyple/example/pc/calypso/PcscReadersSettings.java
+++ keyple-example/pc/src/main/java/org/eclipse/keyple/example/pc/calypso/PcscReadersSettings.java
@@ -11,5 +11,6 @@ package org.eclipse.keyple.example.pc.calypso;
 public interface PcscReadersSettings {
     // This is where you should add patterns of readers you want to use for tests
     String PO_READER_NAME_REGEX = ".*(ASK|ACS).*";
-    String CSM_READER_NAME_REGEX = ".*(Cherry TC|SCM Microsystems|Identive|HID).*";
+    String CSM_READER_NAME_REGEX =
+            ".*(Cherry TC|SCM Microsystems|Identive|HID|Generic                           ).*";
 }
