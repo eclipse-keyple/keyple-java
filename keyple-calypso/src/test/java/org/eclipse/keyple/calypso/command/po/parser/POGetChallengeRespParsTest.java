ffa75572897f13463fffa62c9a988261c09cd00c
OlivierDelcroix <odelcroi@gmail.com>
2018-09-12 10:54:23 +0200

    move package

--- keyple-calypso/src/test/java/org/eclipse/keyple/calypso/command/po/parser/POGetChallengeRespParsTest.java
+++ keyple-calypso/src/test/java/org/eclipse/keyple/calypso/command/po/parser/POGetChallengeRespParsTest.java
@@ -6,13 +6,13 @@
  * available at https://www.eclipse.org/org/documents/epl-2.0/EPL-2.0.html
  */
 
-package org.eclipse.keyple.command.po.parser;
+package org.eclipse.keyple.calypso.command.po.parser;
 
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 import org.eclipse.keyple.calypso.command.po.parser.session.PoGetChallengeRespPars;
-import org.eclipse.keyple.command.AbstractApduResponseParser;
+import org.eclipse.keyple.calypso.command.AbstractApduResponseParser;
 import org.eclipse.keyple.seproxy.ApduResponse;
 import org.eclipse.keyple.seproxy.SeResponse;
 import org.eclipse.keyple.seproxy.SeResponseSet;
