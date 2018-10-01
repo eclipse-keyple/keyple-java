1389306a9042518518e3b14c1a7dc8e0ebc1e307
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2018-10-01 18:18:05 +0200

    Simplified the SeReponseSet/SeReponse transmission with KeypleIOReader/KeypleChannelState Exceptions

--- keyple-core/src/main/java/org/eclipse/keyple/seproxy/exception/KeypleReaderException.java
+++ keyple-core/src/main/java/org/eclipse/keyple/seproxy/exception/KeypleReaderException.java
@@ -15,65 +15,46 @@ import org.eclipse.keyple.seproxy.SeResponseSet;
  * Base Exceptions thrown in a {@link org.eclipse.keyple.seproxy.ProxyReader} context
  */
 public class KeypleReaderException extends KeypleBaseException {
-    private final SeResponseSet seResponseSet;
-    private final SeResponse seResponse;
+    /*
+     * SeResponseSet and SeResponse objects to carry partial responses in the event of a breakdown
+     * in communication with the SE.
+     */
+    private SeResponseSet seResponseSet;
+    private SeResponse seResponse;
 
     /**
      * New exception to be thrown
-     * 
+     *
      * @param message : message to identify the exception and the context
      */
     public KeypleReaderException(String message) {
         super(message);
-        this.seResponseSet = null;
-        this.seResponse = null;
     }
 
     /**
      * Encapsulate a lower level reader exception
-     * 
-     * @param message: message to add some context to the exception
-     * @param cause: lower level exception
+     *
+     * @param message : message to add some context to the exception
+     * @param cause : lower level exception
      */
     public KeypleReaderException(String message, Throwable cause) {
         super(message, cause);
-        this.seResponseSet = null;
-        this.seResponse = null;
     }
 
-    /**
-     * Encapsulate a lower level reader exception but allow the current SeResponseSet to be provided
-     * to the application.
-     * 
-     * @param message: message to add some context to the exception
-     * @param cause: lower level exception
-     * @param seResponseSet: current {@link SeResponseSet}
-     */
-    public KeypleReaderException(String message, Throwable cause, SeResponseSet seResponseSet) {
-        super(message, cause);
-        this.seResponseSet = seResponseSet;
-        this.seResponse = null;
-    }
-
-    /**
-     * Encapsulate a lower level reader exception but allow the current SeResponseSet to be provided
-     * to the application.
-     *
-     * @param message: message to add some context to the exception
-     * @param cause: lower level exception
-     * @param seResponse: current {@link SeResponse}
-     */
-    public KeypleReaderException(String message, Throwable cause, SeResponse seResponse) {
-        super(message, cause);
-        this.seResponseSet = null;
-        this.seResponse = seResponse;
-    }
-
+    /* Setters and Getters for SeResponseSet and SeResponse */
     public SeResponseSet getSeResponseSet() {
         return seResponseSet;
     }
 
+    public void setSeResponseSet(SeResponseSet seResponseSet) {
+        this.seResponseSet = seResponseSet;
+    }
+
     public SeResponse getSeResponse() {
         return seResponse;
+    }
+
+    public void setSeResponse(SeResponse seResponse) {
+        this.seResponse = seResponse;
     }
 }
