e5e6586e9ed77779fec1768a0ec38e8f94665f15
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2018-10-09 18:47:20 +0200

    Updated PoVersionTest to comply with the latest changes

--- keyple-calypso/src/test/java/org/eclipse/keyple/calypso/command/po/PoVersionTest.java
+++ keyple-calypso/src/test/java/org/eclipse/keyple/calypso/command/po/PoVersionTest.java
@@ -9,7 +9,9 @@
 package org.eclipse.keyple.calypso.command.po;
 
 
-import org.eclipse.keyple.calypso.transaction.PoSecureSession;
+import org.eclipse.keyple.calypso.command.po.parser.GetDataFciRespPars;
+import org.eclipse.keyple.seproxy.ApduResponse;
+import org.eclipse.keyple.util.ByteArrayUtils;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,20 +20,33 @@ import org.mockito.junit.MockitoJUnitRunner;
 @RunWith(MockitoJUnitRunner.class)
 public class PoVersionTest {
 
-    @Test
-    public void computePoRevision() {
-
-        // 01h to 04h Calypso Rev 1 or Rev 2 (depending on Application Subtype)
-        // 06h to 1Fh Calypso Rev 2
-        // 20h to 7Fh Calypso Rev.3
-        Assert.assertEquals(PoSecureSession.computePoRevision((byte) 0x01), PoRevision.REV2_4);
-        Assert.assertEquals(PoSecureSession.computePoRevision((byte) 0x04), PoRevision.REV2_4);
-        Assert.assertEquals(PoSecureSession.computePoRevision((byte) 0x06), PoRevision.REV2_4);
-        Assert.assertEquals(PoSecureSession.computePoRevision((byte) 0x1F), PoRevision.REV2_4);
-        Assert.assertEquals(PoSecureSession.computePoRevision((byte) 0x20), PoRevision.REV3_1);
-        Assert.assertEquals(PoSecureSession.computePoRevision((byte) 0x27), PoRevision.REV3_1);
-        Assert.assertEquals(PoSecureSession.computePoRevision((byte) 0x28), PoRevision.REV3_2);
-        Assert.assertEquals(PoSecureSession.computePoRevision((byte) 0x2F), PoRevision.REV3_2);
+    /* Building FCI data with the application byte as a variant */
+    private static class BuildFci {
+        private static ApduResponse getFci(byte applicationByte) {
+            return new ApduResponse(ByteArrayUtils.fromHex(String
+                    .format("6F 22 84 08 315449432E494341" + "A5 16 BF0C 13 C7 08 0000000011223344"
+                            + "53 07 060A %02X 02200311 9000", applicationByte)),
+                    null);
+        }
     }
 
+    @Test
+    public void computePoRevision() {
+        Assert.assertEquals(new GetDataFciRespPars(BuildFci.getFci((byte) 0x01)).getPoRevision(),
+                PoRevision.REV2_4);
+        Assert.assertEquals(new GetDataFciRespPars(BuildFci.getFci((byte) 0x04)).getPoRevision(),
+                PoRevision.REV2_4);
+        Assert.assertEquals(new GetDataFciRespPars(BuildFci.getFci((byte) 0x06)).getPoRevision(),
+                PoRevision.REV2_4);
+        Assert.assertEquals(new GetDataFciRespPars(BuildFci.getFci((byte) 0x1F)).getPoRevision(),
+                PoRevision.REV2_4);
+        Assert.assertEquals(new GetDataFciRespPars(BuildFci.getFci((byte) 0x20)).getPoRevision(),
+                PoRevision.REV3_1);
+        Assert.assertEquals(new GetDataFciRespPars(BuildFci.getFci((byte) 0x27)).getPoRevision(),
+                PoRevision.REV3_1);
+        Assert.assertEquals(new GetDataFciRespPars(BuildFci.getFci((byte) 0x28)).getPoRevision(),
+                PoRevision.REV3_2);
+        Assert.assertEquals(new GetDataFciRespPars(BuildFci.getFci((byte) 0x2F)).getPoRevision(),
+                PoRevision.REV3_2);
+    }
 }
