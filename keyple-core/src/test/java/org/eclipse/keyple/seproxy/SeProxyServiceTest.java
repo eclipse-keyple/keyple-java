4e031aca5c72c1ed38e6cdf2bd4b34520a2afebe
OlivierDelcroix <odelcroi@gmail.com>
2018-10-15 19:16:43 +0200

    appli spotless with new license copy notice and license to jar/META-INF

--- keyple-core/src/test/java/org/eclipse/keyple/seproxy/SeProxyServiceTest.java
+++ keyple-core/src/test/java/org/eclipse/keyple/seproxy/SeProxyServiceTest.java
@@ -1,9 +1,12 @@
 /*
  * Copyright (c) 2018 Calypso Networks Association https://www.calypsonet-asso.org/
  *
- * All rights reserved. This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License version 2.0 which accompanies this distribution, and is
- * available at https://www.eclipse.org/org/documents/epl-2.0/EPL-2.0.html
+ * This program and the accompanying materials are made available under the terms of the Eclipse
+ * Public License version 2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/org/documents/epl-2.0/EPL-2.0.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
  */
 
 package org.eclipse.keyple.seproxy;
