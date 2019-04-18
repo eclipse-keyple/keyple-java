0611bc5a9b414bb7fdae650439d81675072c89a1
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2019-04-18 13:17:06 +0200

    Completed CalypsoPo unit tests

--- java/component/keyple-calypso/src/test/java/org/eclipse/keyple/calypso/transaction/CalypsoPoTest.java
+++ java/component/keyple-calypso/src/test/java/org/eclipse/keyple/calypso/transaction/CalypsoPoTest.java
@@ -28,25 +28,76 @@ import org.mockito.junit.MockitoJUnitRunner;
 public class CalypsoPoTest {
     private final static String ATR_VALUE = "3B8F8001805A08030400020011223344829000F3";
     private final static String ATR_VALUE_2 = "3B8F8001805A08030400020011223344829000";
+    /*
+    // @formatter:off
+    Rev 3.1 FCI sample data
+    6F
+    23
+        84
+        09
+        315449432E49434131      // AID
+        A5
+        16
+            BF0C
+            13
+                C7
+                08
+                0000000011223344 // SERIAL NUMBER
+                53
+                07
+                0A3C2305141001   // STARTUP INFORMATION
+
+      STARTUP INFORMATION
+          0A Buffer size indicator
+          3C Type of platform
+          23 Calypso revision
+          05 File structure reference
+          14 Software issuer reference
+          10 Software version (MSB)
+          01 Software version (LSB)
+     // @formatter:on
+     */
+    private final static String FCI_REV31 =
+            "6F238409315449432E49434131A516BF0C13C708000000001122334453070A3C23051410019000";
     private final static String DF_NAME = "315449432E494341";
     private final static String SERIAL_NUMBER = "0000000011223344";
 
-    /* Building FCI data with the application byte as a variant and initialize PO */
-    public static CalypsoPo getPoApplicationByte(byte applicationByte) {
-        AnswerToReset atr = new AnswerToReset(ByteArrayUtil.fromHex(ATR_VALUE));
-        ApduResponse fciData =
-                new ApduResponse(
-                        ByteArrayUtil
-                                .fromHex(String.format(
-                                        "6F 22 84 08 " + DF_NAME + "A5 16 BF0C 13 C7 08 "
-                                                + SERIAL_NUMBER + "53 07 060A %02X 02200311 9000",
-                                        applicationByte)),
-                        null);
+    /**
+     * Build a CalypsoPo from atr and fci provided as hex strings
+     * 
+     * @param atrStr the PO ATR
+     * @param fciStr the FCI data
+     * @return a CalypsoPo object
+     */
+    public static CalypsoPo getCalypsoPo(String atrStr, String fciStr) {
+
+        AnswerToReset atr;
+        if (atrStr == null || atrStr.length() == 0) {
+            atr = null;
+        } else {
+            atr = new AnswerToReset(ByteArrayUtil.fromHex(atrStr));
+        }
+
+        ApduResponse fci;
+        if (fciStr == null || fciStr.length() == 0) {
+            fci = new ApduResponse(ByteArrayUtil.fromHex("6700"), null);;
+        } else {
+            fci = new ApduResponse(ByteArrayUtil.fromHex(fciStr), null);
+        }
+
         SeResponse selectionData =
-                new SeResponse(true, false, new SelectionStatus(atr, fciData, true), null);
+                new SeResponse(true, false, new SelectionStatus(atr, fci, true), null);
         CalypsoPo calypsoPo = new CalypsoPo(selectionData, null);
         return calypsoPo;
     }
+
+    /* Building FCI data with the application byte as a variant and initialize PO */
+    public static CalypsoPo getPoApplicationByte(byte applicationByte) {
+        String fciStr = String.format("6F 22 84 08 " + DF_NAME + "A5 16 BF0C 13 C7 08 "
+                + SERIAL_NUMBER + "53 07 060A %02X 02200311 9000", applicationByte);
+        return getCalypsoPo(ATR_VALUE, fciStr);
+    }
+
 
     @Test
     public void getRevision() {
@@ -136,5 +187,36 @@ public class CalypsoPoTest {
         Assert.assertFalse(calypsoPo.isModificationsCounterInBytes());
         Assert.assertEquals(3, calypsoPo.getModificationsCounter());
         Assert.assertEquals(PoClass.LEGACY, calypsoPo.getPoClass());
+    }
+
+    @Test
+    public void otherAttributes() {
+        CalypsoPo calypsoPo;
+        /* Rev1 PO */
+        calypsoPo = getCalypsoPo(ATR_VALUE, "6700");
+        Assert.assertFalse(calypsoPo.isModificationsCounterInBytes());
+        Assert.assertEquals(3, calypsoPo.getModificationsCounter());
+        Assert.assertEquals(0, calypsoPo.getBufferSizeIndicator());
+        Assert.assertEquals(3, calypsoPo.getModificationsCounter());
+        Assert.assertEquals(3, calypsoPo.getBufferSizeValue());
+        Assert.assertEquals(0x08, calypsoPo.getPlatformByte());
+        Assert.assertEquals(0x03, calypsoPo.getApplicationTypeByte());
+        Assert.assertEquals(0x04, calypsoPo.getApplicationSubtypeByte());
+        Assert.assertEquals(0x00, calypsoPo.getSoftwareIssuerByte());
+        Assert.assertEquals(0x02, calypsoPo.getSoftwareVersionByte());
+        Assert.assertEquals(0x00, calypsoPo.getSoftwareRevisionByte());
+
+        /* Rev 3.1 */
+        calypsoPo = getCalypsoPo(ATR_VALUE, FCI_REV31);
+        Assert.assertTrue(calypsoPo.isModificationsCounterInBytes());
+        Assert.assertEquals(10, calypsoPo.getBufferSizeIndicator());
+        Assert.assertEquals(430, calypsoPo.getModificationsCounter());
+        Assert.assertEquals(430, calypsoPo.getBufferSizeValue());
+        Assert.assertEquals(0x3C, calypsoPo.getPlatformByte());
+        Assert.assertEquals(0x23, calypsoPo.getApplicationTypeByte());
+        Assert.assertEquals(0x05, calypsoPo.getApplicationSubtypeByte());
+        Assert.assertEquals(0x14, calypsoPo.getSoftwareIssuerByte());
+        Assert.assertEquals(0x10, calypsoPo.getSoftwareVersionByte());
+        Assert.assertEquals(0x01, calypsoPo.getSoftwareRevisionByte());
     }
 }
