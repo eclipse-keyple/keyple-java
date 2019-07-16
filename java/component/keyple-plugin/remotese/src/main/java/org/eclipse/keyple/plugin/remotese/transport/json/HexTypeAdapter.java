c816920c80a5d343c16f4b04b1ee242ffece910c
OlivierDelcroix <odelcroi@gmail.com>
2019-07-16 09:56:07 +0200

    inline Dto with hexadecimal values
    Signed-off-by: OlivierDelcroix <odelcroi@gmail.com>

---
+++ java/component/keyple-plugin/remotese/src/main/java/org/eclipse/keyple/plugin/remotese/transport/json/HexTypeAdapter.java
@@ -0,0 +1,29 @@
+/********************************************************************************
+ * Copyright (c) 2019 Calypso Networks Association https://www.calypsonet-asso.org/
+ *
+ * See the NOTICE file(s) distributed with this work for additional information regarding copyright
+ * ownership.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse
+ * Public License 2.0 which is available at http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ ********************************************************************************/
+package org.eclipse.keyple.plugin.remotese.transport.json;
+
+import java.lang.reflect.Type;
+import org.eclipse.keyple.core.util.ByteArrayUtil;
+import com.google.gson.*;
+
+public class HexTypeAdapter implements JsonSerializer<byte[]>, JsonDeserializer<byte[]> {
+
+    public byte[] deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+            throws JsonParseException {
+        return ByteArrayUtil.fromHex(json.getAsString());
+    }
+
+    public JsonElement serialize(byte[] data, Type typeOfSrc, JsonSerializationContext context) {
+        return new JsonPrimitive(ByteArrayUtil.toHex(data));
+    }
+
+}
