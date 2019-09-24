7fdf4acab6cd72ccfcd4b047898fc816f700392e
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2019-09-24 12:31:57 +0200

    Renamed SmartReader interface to SmartRemovalReader

--- java/component/keyple-core/src/main/java/org/eclipse/keyple/core/seproxy/plugin/SmartRemovalReader.java
+++ java/component/keyple-core/src/main/java/org/eclipse/keyple/core/seproxy/plugin/SmartRemovalReader.java
@@ -12,31 +12,30 @@
 package org.eclipse.keyple.core.seproxy.plugin;
 
 import org.eclipse.keyple.core.seproxy.event.ObservableReader;
-import org.eclipse.keyple.core.seproxy.exception.NoStackTraceThrowable;
 
 /**
  * Interface implemented by the readers able to handle natively the card removal process.
- * <p>
  */
-public interface SmartReader extends ObservableReader {
+public interface SmartRemovalReader extends ObservableReader {
     /**
      * Wait until the card disappears.
      * <p>
      * This method must be implemented by the plugin's reader class when it implements the
-     * {@link SmartReader} interface. The reader implementation must manage the card withdrawal
-     * process itself. (for example by using the analogous waitForCardAbsent method in the case of a
-     * plugin based on smartcard.io [PC/SC]).
+     * {@link SmartRemovalReader} interface. The reader implementation must manage the card
+     * withdrawal process itself. (for example by using the analogous waitForCardAbsent method in
+     * the case of a plugin based on smartcard.io [PC/SC]).
      * <p>
      * In the case where the reader plugin is not able to handle the card removal process itself
-     * (not implementing the {@link SmartReader} interface, then it is managed by the
+     * (not implementing the {@link SmartRemovalReader} interface, then it is managed by the
      * waitForCardAbsentPing method defined in this class.
      * <p>
-     * Returns true if a card has disappeared before the end of the provided timeout. Returns false
-     * if thecd .. is still present within the delay.
+     * Returns true if a card has disappeared before the end of the provided timeout.
+     * <p>
+     * Returns false if the SE is still present within the delay.
      *
      * @param timeout the delay in millisecond we wait for a card to be withdrawn, a value of zero
      *        means wait for ever.
      * @return presence status
      */
-    boolean waitForCardAbsentNative(long timeout) throws NoStackTraceThrowable;
+    boolean waitForCardAbsentNative(long timeout);
 }
