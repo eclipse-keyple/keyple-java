6e40ffd5afeebdd247713d30b8ba644bbc1c57bf
Jeremy Vagnet <jeremy@curious-company.com>
2019-10-08 10:43:56 +0200

    use the correct version of PcscReadersSettings.java
    Signed-off-by: Jeremy Vagnet <jeremy@curious-company.com>

--- java/example/common/src/main/java/org/eclipse/keyple/example/common/PcscReadersSettings.java
+++ java/example/common/src/main/java/org/eclipse/keyple/example/common/PcscReadersSettings.java
@@ -14,4 +14,5 @@ package org.eclipse.keyple.example.generic.pc;
 public interface PcscReadersSettings {
     // This is where you should add patterns of readers you want to use for tests
     String PO_READER_NAME_REGEX = ".*(ASK|ACS).*";
+    String SAM_READER_NAME_REGEX = ".*(Cherry TC|SCM Microsystems|Identive|HID|Generic).*";
 }
