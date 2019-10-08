f74e6eccc399fe065f57813a65a1860dac1afacd
Jeremy Vagnet <jeremy@curious-company.com>
2019-10-08 15:08:40 +0200

    working and proper reorg for UseCase_Generic1_ExplicitSelectionAid
    Signed-off-by: Jeremy Vagnet <jeremy@curious-company.com>

--- java/example/generic/pc/UseCase_Generic1_ExplicitSelectionAid/src/main/java/org/eclipse/keyple/example/generic/pc/UseCase_Generic1_ExplicitSelectionAid/UseCase_Generic1_ExplicitSelectionAid_Pcsc.java
+++ java/example/generic/pc/UseCase_Generic1_ExplicitSelectionAid/src/main/java/org/eclipse/keyple/example/generic/pc/UseCase_Generic1_ExplicitSelectionAid/UseCase_Generic1_ExplicitSelectionAid_Pcsc.java
@@ -9,7 +9,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  ********************************************************************************/
-package org.eclipse.keyple.example.generic.pc;
+package org.eclipse.keyple.example.generic.pc.UseCase_Generic1_ExplicitSelectionAid;
 
 
 import java.io.IOException;
@@ -22,7 +22,8 @@ import org.eclipse.keyple.core.seproxy.exception.KeypleBaseException;
 import org.eclipse.keyple.core.seproxy.exception.NoStackTraceThrowable;
 import org.eclipse.keyple.core.seproxy.protocol.SeCommonProtocols;
 import org.eclipse.keyple.core.util.ByteArrayUtil;
-import org.eclipse.keyple.example.generic.common.GenericSeSelectionRequest;
+import org.eclipse.keyple.example.common.ReaderUtilities;
+import org.eclipse.keyple.example.common.generic.GenericSeSelectionRequest;
 import org.eclipse.keyple.plugin.pcsc.PcscPluginFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
