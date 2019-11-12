a453c597db2ff709b5ccfdd96b27ccb7fa1d6b0f
OlivierDelcroix <odelcroi@gmail.com>
2019-11-12 14:03:25 +0100

    spotless
    Signed-off-by: OlivierDelcroix <odelcroi@gmail.com>

--- java/component/keyple-core/src/test/java/org/eclipse/keyple/core/seproxy/plugin/local/WaitForSeRemovalJobExecutorTest.java
+++ java/component/keyple-core/src/test/java/org/eclipse/keyple/core/seproxy/plugin/local/WaitForSeRemovalJobExecutorTest.java
@@ -14,7 +14,6 @@ package org.eclipse.keyple.core.seproxy.plugin.local;
 import static org.eclipse.keyple.core.seproxy.plugin.local.AbstractObservableState.MonitoringState.WAIT_FOR_SE_INSERTION;
 import static org.eclipse.keyple.core.seproxy.plugin.local.AbstractObservableState.MonitoringState.WAIT_FOR_START_DETECTION;
 import static org.mockito.Mockito.*;
-
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import org.eclipse.keyple.core.CoreBaseTest;
