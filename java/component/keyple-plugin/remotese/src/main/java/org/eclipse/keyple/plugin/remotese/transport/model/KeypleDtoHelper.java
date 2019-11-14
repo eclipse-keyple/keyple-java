889219eb8acea4b79a96e1b8d4f5f638c24029f8
OlivierDelcroix <odelcroi@gmail.com>
2019-11-14 12:27:25 +0100

    javadoc
    Signed-off-by: OlivierDelcroix <odelcroi@gmail.com>

--- java/component/keyple-plugin/remotese/src/main/java/org/eclipse/keyple/plugin/remotese/transport/model/KeypleDtoHelper.java
+++ java/component/keyple-plugin/remotese/src/main/java/org/eclipse/keyple/plugin/remotese/transport/model/KeypleDtoHelper.java
@@ -16,19 +16,51 @@ import org.eclipse.keyple.plugin.remotese.transport.json.JsonParser;
 import com.google.gson.JsonObject;
 
 /**
- * Utility class to manipulate KeypleDto
+ * Utility class to manipulate KeypleDto. Use this class to build the different types of {@link KeypleDto}
+ * <ul>
+ *    <li>Request</li>
+ *    <li>Response</li>
+ *    <li>Notification</li>
+ *      <li>NoResponse</li>
+ *      <li>Exception</li>
+ *</ul>
  */
 public class KeypleDtoHelper {
 
 
-    static private KeypleDto build(String action, String body, boolean isRequest, String sessionId,
-            String nativeReaderName, String virtualReaderName, String requesterNodeId,
-            String targetNodeId, String id) {
-        return new KeypleDto(action, body, isRequest, sessionId, nativeReaderName,
-                virtualReaderName, requesterNodeId, targetNodeId, id);
+    /* ----------- Constructors Helpers */
+
+    /**
+     * Build a KeypleDto of type "Request"
+     * @param action : name of the remote method
+     * @param body : parameters of the remote method
+     * @param sessionId : virtual session id (if exists)
+     * @param nativeReaderName : name of the local reader
+     * @param virtualReaderName : name of the virtual reader (if exists)
+     * @param requesterNodeId : node id of the sender
+     * @param targetNodeId : node id of the destinee
+     * @param id : unique id for this request
+     * @return keypleDto request
+     */
+    static public KeypleDto buildRequest(String action, String body, String sessionId,
+                                         String nativeReaderName, String virtualReaderName, String requesterNodeId,
+                                         String targetNodeId, String id) {
+        return new KeypleDto(action, body, true, sessionId, nativeReaderName, virtualReaderName,
+                requesterNodeId, targetNodeId, id);
     }
 
-
+    /**
+     * Build a KeypleDto of type "Response"
+     * @param action : name of the remote method
+     * @param body : parameters of the remote method
+     * @param sessionId : virtual session id (if exists)
+     * @param nativeReaderName : name of the local reader
+     * @param virtualReaderName : name of the virtual reader (if exists)
+     * @param requesterNodeId : node id of the sender
+     * @param targetNodeId : node id of the destinee
+     * @param id : id of the request
+     * @return keypleDto response
+     */
     static public KeypleDto buildResponse(String action, String body, String sessionId,
             String nativeReaderName, String virtualReaderName, String requesterNodeId,
             String targetNodeId, String id) {
@@ -36,13 +68,17 @@ public class KeypleDtoHelper {
                 requesterNodeId, targetNodeId, id);
     }
 
-    static public KeypleDto buildRequest(String action, String body, String sessionId,
-            String nativeReaderName, String virtualReaderName, String requesterNodeId,
-            String targetNodeId, String id) {
-        return new KeypleDto(action, body, true, sessionId, nativeReaderName, virtualReaderName,
-                requesterNodeId, targetNodeId, id);
-    }
-
+    /**
+     * Build a KeypleDto of type "Notification", (without id)
+     * @param action : name of the notification
+     * @param body : parameters of the notification
+     * @param sessionId : virtual session id
+     * @param nativeReaderName : name of the local reader
+     * @param virtualReaderName : name of the virtual reader
+     * @param requesterNodeId : node id of the sender
+     * @param targetNodeId : node id of the destinee
+     * @return keypleDto notification
+     */
     static public KeypleDto buildNotification(String action, String body, String sessionId,
             String nativeReaderName, String virtualReaderName, String requesterNodeId,
             String targetNodeId) {
@@ -50,49 +86,107 @@ public class KeypleDtoHelper {
                 requesterNodeId, targetNodeId, null);
     }
 
-    static public String toJson(KeypleDto keypleDto) {
-        return JsonParser.getGson().toJson(keypleDto);
-    }
-
-    static public KeypleDto fromJson(String json) {
-        return JsonParser.getGson().fromJson(json, KeypleDto.class);
-    }
-
-    static public KeypleDto fromJsonObject(JsonObject jsonObj) {
-        return JsonParser.getGson().fromJson(jsonObj, KeypleDto.class);
-    }
-
-
-
-    static public KeypleDto NoResponse(String id) {
-        return buildResponse("", "", "", "", "", "", "", id);
-    }
-
+    /**
+     * Build a keypleDto of type "Exception" containing a Java Throwable
+     * <p>
+     *     This keypleDto send the exception to the other node
+     * @param action : name of the remote method that failed
+     * @param sessionId : virtual session id (if exists)
+     * @param nativeReaderName : name of the local reader
+     * @param virtualReaderName : name of the virtual reader (if exists)
+     * @param requesterNodeId : node id of the sender
+     * @param targetNodeId : node id of the destinee
+     * @param id
+     * @return
+     */
     static public KeypleDto ExceptionDTO(String action, Throwable exception, String sessionId,
-            String nativeReaderName, String virtualReaderName, String requesterNodeId,
-            String targetNodeId, String id) {
+                                         String nativeReaderName, String virtualReaderName, String requesterNodeId,
+                                         String targetNodeId, String id) {
 
         return buildResponse(action, JsonParser.getGson().toJson(exception), sessionId,
                 nativeReaderName, virtualReaderName, requesterNodeId, targetNodeId, id);
     }
 
+    /**
+     * Build a keypleDto of type "NoResponse".
+     * This Dto should be not be sent by the {@link org.eclipse.keyple.plugin.remotese.transport.DtoNode}.
+     * @param id : id of the request
+     * @return NoResponse KeypleDto
+     */
+    static public KeypleDto NoResponse(String id) {
+        return buildResponse("", "", "", "", "", "", "", id);
+    }
+
+    /**
+     * Check if the keypleDto is of type "NoResponse"
+     * @param dto
+     * @return true of the keypleDto is of type "NoResponse"
+     */
     static public Boolean isNoResponse(KeypleDto dto) {
         return dto == null || dto.getAction() == null || dto.getAction().isEmpty();
     }
 
+    /**
+     * Check if the keypleDto is of type "Exception"
+     * @param keypleDto
+     * @return true of the keypleDto is of type "Exception"
+     */
+    static public Boolean containsException(KeypleDto keypleDto) {
+        return keypleDto.getBody().contains("stackTrace");
+    }
 
+
+
+
+    /* ----------- Serialization Helpers */
+
+    /**
+     * Serialize keypleDto to json
+     * @param keypleDto
+     * @return json serialization of the keypleDto
+     */
+    static public String toJson(KeypleDto keypleDto) {
+        return JsonParser.getGson().toJson(keypleDto);
+    }
+
+    /**
+     * Parse a json serialized keypleDto
+     * @param json json String representation of the keypleDto
+     * @return keypleDto object
+     */
+    static public KeypleDto fromJson(String json) {
+        return JsonParser.getGson().fromJson(json, KeypleDto.class);
+    }
+
+    /**
+     * Parse a json keypleDto
+     * @param jsonObj json Object representation of the keypleDto
+     * @return keypleDto object
+     */
+    static public KeypleDto fromJsonObject(JsonObject jsonObj) {
+        return JsonParser.getGson().fromJson(jsonObj, KeypleDto.class);
+    }
+
+    /**
+     * The parameter in the keypleDto has not been specified
+     * @return
+     */
+    static public String notSpecified() {
+        return "notSpecified";
+    }
 
     static public Boolean isKeypleDTO(JsonObject json) {
         return json.has("action");
     }
 
-    static public Boolean containsException(KeypleDto keypleDto) {
-        return keypleDto.getBody().contains("stackTrace");
-    }
 
-    static public String notSpecified() {
-        return "notSpecified";
-    }
+    /* --------- private method ---------- */
 
+    static private KeypleDto build(String action, String body, boolean isRequest, String sessionId,
+                                   String nativeReaderName, String virtualReaderName, String requesterNodeId,
+                                   String targetNodeId, String id) {
+        return new KeypleDto(action, body, isRequest, sessionId, nativeReaderName,
+                virtualReaderName, requesterNodeId, targetNodeId, id);
+    }
 
 }
