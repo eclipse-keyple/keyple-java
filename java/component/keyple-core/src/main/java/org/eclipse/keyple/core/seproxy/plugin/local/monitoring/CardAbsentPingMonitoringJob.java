94daa1b327ff00ebcb966e8fad06240be7e5886d
OlivierDelcroix <odelcroi@gmail.com>
2019-12-11 15:07:30 +0100

    add removalWait in constructor
    Signed-off-by: OlivierDelcroix <odelcroi@gmail.com>

--- java/component/keyple-core/src/main/java/org/eclipse/keyple/core/seproxy/plugin/local/monitoring/CardAbsentPingMonitoringJob.java
+++ java/component/keyple-core/src/main/java/org/eclipse/keyple/core/seproxy/plugin/local/monitoring/CardAbsentPingMonitoringJob.java
@@ -28,7 +28,7 @@ import org.slf4j.LoggerFactory;
  * It is based on sending a neutral APDU command as long as the SE is responding, an internal
  * SE_REMOVED event is fired when the SE is no longer responding.
  * <p>
- * A delay of 200 ms is inserted between each APDU sending.
+ * By default a delay of 200 ms is inserted between each APDU sending .
  */
 public class CardAbsentPingMonitoringJob implements MonitoringJob {
 
@@ -37,10 +37,26 @@ public class CardAbsentPingMonitoringJob implements MonitoringJob {
     private final AbstractObservableLocalReader reader;
     private Runnable job;
     Boolean loop;
+    private long removalWait = 200;
 
+    /**
+     * Create a job monitor job that ping the SE with the method isSePresentPing()
+     * @param reader : reference to the reader
+     */
     public CardAbsentPingMonitoringJob(AbstractObservableLocalReader reader) {
         this.reader = reader;
         this.loop = true;
+    }
+
+    /**
+     * Create a job monitor job that ping the SE with the method isSePresentPing()
+     * @param reader : reference to the reader
+     * @param removalWait : delay between between each APDU sending
+     */
+    public CardAbsentPingMonitoringJob(AbstractObservableLocalReader reader, long removalWait) {
+        this.reader = reader;
+        this.loop = true;
+        this.removalWait = removalWait;
     }
 
     @Override
@@ -52,7 +68,6 @@ public class CardAbsentPingMonitoringJob implements MonitoringJob {
          * failed - InterruptedException is caught
          */
         job = new Runnable() {
-            long threshold = 200;
             long retries = 0;
 
             @Override
@@ -72,7 +87,7 @@ public class CardAbsentPingMonitoringJob implements MonitoringJob {
                     }
                     try {
                         // wait a bit
-                        Thread.sleep(threshold);
+                        Thread.sleep(removalWait);
                     } catch (InterruptedException ignored) {
                         // Restore interrupted state...      
                         Thread.currentThread().interrupt();
