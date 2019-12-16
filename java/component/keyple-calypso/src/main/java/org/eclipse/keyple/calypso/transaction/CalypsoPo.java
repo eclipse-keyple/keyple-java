466fb67b183e9bd70cde91349a2b02b9220a0d2e
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2019-12-16 14:31:39 +0100

    Added PoCommandsManagerTest unit test class

--- java/component/keyple-calypso/src/main/java/org/eclipse/keyple/calypso/transaction/CalypsoPo.java
+++ java/component/keyple-calypso/src/main/java/org/eclipse/keyple/calypso/transaction/CalypsoPo.java
@@ -25,13 +25,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * The CalypsoPo handles the Calypso SE characteristics such as:
+ * The CalypsoPo class gathers all the information about the current PO retrieved from the response
+ * to the select application command.
+ * <p>
+ * An instance of CalypsoPo can be obtained by casting the AbstractMatchingSe object from the
+ * selection process (e.g. (CalypsoPo) matchingSelection.getMatchingSe())
+ * <p>
+ * The various information contained in CalypsoPo is accessible by getters and includes:
  * <ul>
- * <li>revision</li>
- * <li>serial number</li>
- * <li>session buffer limit</li>
+ * <li>The application identification fields (revision/version, class, DF name, serial number, ATR,
+ * issuer)
+ * <li>The indication of the presence of optional features (Stored Value, PIN, Rev3.2 mode,
+ * ratification management)
+ * <li>The management information of the modification buffer
+ * <li>The invalidation status
  * </ul>
- * TODO Complete with other PO features from the FCI and/or ATR
  */
 public final class CalypsoPo extends AbstractMatchingSe {
     private static final Logger logger = LoggerFactory.getLogger(CalypsoPo.class);
@@ -169,83 +177,234 @@ public final class CalypsoPo extends AbstractMatchingSe {
         }
     }
 
+    /**
+     * The PO revision indicates the generation of the product presented.
+     * <p>
+     * It will also have an impact on the internal construction of some commands to take into
+     * account the specificities of the different POs.
+     * 
+     * @return an enum giving the identified PO revision
+     */
     public PoRevision getRevision() {
         return this.revision;
     }
 
+    /**
+     * The DF name is the name of the application DF as defined in ISO/IEC 7816-4.
+     * <p>
+     * It also corresponds to the complete representation of the target covered by the AID value
+     * provided in the selection command.
+     * <p>
+     * The AID selects the application by specifying all or part of the targeted DF Name (5 bytes
+     * minimum).
+     * 
+     * @return a byte array containing the DF Name bytes (5 to 16 bytes)
+     */
     public byte[] getDfName() {
         return dfName;
     }
 
+    /**
+     * The serial number for the application, is unique ID for the PO.
+     * <p>
+     * It is also used for key derivation.
+     * 
+     * @return a byte array containing the Application Serial Number (8 bytes)
+     */
     public byte[] getApplicationSerialNumber() {
         return applicationSerialNumber;
     }
 
+    /**
+     * The Answer To Reset is sent by the PO is ISO7816-3 mode and in contactless mode for PC/SC
+     * readers.
+     * <p>
+     * When the ATR is obtained in contactless mode, it is in fact reconstructed by the reader from
+     * information obtained from the lower communication layers. Therefore, it may differ from one
+     * reader to another depending on the interpretation that has been made by the manufacturer of
+     * the PC/SC standard.
+     * <p>
+     * This field is not interpreted in the Calypso module.
+     * 
+     * @return a byte array containing the ATR (variable length)
+     */
     public byte[] getAtr() {
         return poAtr;
     }
 
+    /**
+     * Specifies whether the change counter allowed in session is established in number of
+     * operations or number of bytes modified.
+     * <p>
+     * This varies depending on the revision of the PO.
+     * 
+     * @return true if the counter is number of bytes
+     */
     public boolean isModificationsCounterInBytes() {
         return modificationCounterIsInBytes;
     }
 
+    /**
+     * Indicates the maximum number of changes allowed in session.
+     * <p>
+     * This number can be a number of operations or a number of commands (see
+     * isModificationsCounterInBytes)
+     * 
+     * @return the maximum number of modifications allowed
+     */
     public int getModificationsCounter() {
         return modificationsCounterMax;
     }
 
+    /**
+     * This field is directly from the Startup Information zone of the PO.
+     * <p>
+     * When the modification counter is in number of operations, it is the maximum number of
+     * operations allowed.
+     * <p>
+     * When the modification counter is in bytes, it is used to determine the maximum number of
+     * modified bytes allowed. (see the formula in the PO specification)
+     * 
+     * @return the buffer size indicator byte
+     */
     public byte getBufferSizeIndicator() {
         return bufferSizeIndicator;
     }
 
+    /**
+     * The buffer size value is the raw interpretation of the buffer size indicator to provide a
+     * number of bytes.
+     * <p>
+     * The revision number must be taken into account at the same time to be accurate.
+     * <p>
+     * It is better to use getModificationsCounter and isModificationsCounterInBytes
+     * 
+     * @return the buffer size value evaluated from the buffer size indicator
+     */
     public int getBufferSizeValue() {
         return bufferSizeValue;
     }
 
+    /**
+     * The platform identification byte is the reference of the chip
+     * 
+     * @return the platform identification byte
+     */
     public byte getPlatformByte() {
         return platform;
     }
 
+    /**
+     * The Application Type byte determines the Calypso Revision and various options
+     * 
+     * @return the Application Type byte
+     */
     public byte getApplicationTypeByte() {
         return applicationType;
     }
 
+    /**
+     * Indicates whether the 3.2 mode is supported or not.
+     * <p>
+     * This boolean is interpreted from the Application Type byte
+     * 
+     * @return true if the revision 3.2 mode is supported
+     */
     public boolean isRev3_2ModeAvailable() {
         return isRev3_2ModeAvailable;
     }
 
+    /**
+     * Indicates if the ratification is done on deselect (ratification command not necessary)
+     * <p>
+     * This boolean is interpreted from the Application Type byte
+     * 
+     * @return true if the ratification command is required
+     */
     public boolean isRatificationCommandRequired() {
         return isRatificationCommandRequired;
     }
 
+    /**
+     * Indicates whether the PO has the Calypso Stored Value feature.
+     * <p>
+     * This boolean is interpreted from the Application Type byte
+     * 
+     * @return true if the PO has the Stored Value feature
+     */
     public boolean hasCalypsoStoredValue() {
         return hasCalypsoStoredValue;
     }
 
+    /**
+     * Indicates whether the PO has the Calypso PIN feature.
+     * <p>
+     * This boolean is interpreted from the Application Type byte
+     * 
+     * @return true if the PO has the PIN feature
+     */
     public boolean hasCalypsoPin() {
         return hasCalypsoPin;
     }
 
+    /**
+     * The Application Subtype indicates to the terminal a reference to the file structure of the
+     * Calypso DF.
+     * 
+     * @return the Application Subtype byte
+     */
     public byte getApplicationSubtypeByte() {
         return applicationSubtypeByte;
     }
 
+    /**
+     * The Software Issuer byte indicates the entity responsible for the software of the selected
+     * application.
+     * 
+     * @return the Software Issuer byte
+     */
     public byte getSoftwareIssuerByte() {
         return softwareIssuerByte;
     }
 
+    /**
+     * The Software Version field may be set to any fixed value by the Software Issuer of the
+     * Calypso application.
+     * 
+     * @return the Software Version byte
+     */
     public byte getSoftwareVersionByte() {
         return softwareVersion;
     }
 
+    /**
+     * The Software Revision field may be set to any fixed value by the Software Issuer of the
+     * Calypso application.
+     * 
+     * @return the Software Revision byte
+     */
     public byte getSoftwareRevisionByte() {
         return softwareRevision;
     }
 
+    /**
+     * Indicated whether the PO has been invalidated or not.
+     * <p>
+     * An invalidated PO has 6283 as status word in response to the Select Application command.
+     * 
+     * @return true if the PO has been invalidated.
+     */
     public boolean isDfInvalidated() {
         return isDfInvalidated;
     }
 
     /**
+     * The PO class is the ISO7816 class to be used with the current PO.
+     * <p>
+     * It determined from the PO revision
+     * <p>
+     * Two classes are possible: LEGACY and ISO.
+     * 
      * @return the PO class determined from the PO revision
      */
     public PoClass getPoClass() {
