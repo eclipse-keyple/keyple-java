5e3ff1e5d44a59a14ea7eeb397b8cf75ffc63dfa
Meddy Menzikoff <m.menzikoff@oneclickflare.fr>
2020-03-11 17:06:49 +0100

    Feat: Improved Javadoc
    Signed-off-by: Meddy Menzikoff <m.menzikoff@oneclickflare.fr>

--- android/keyple-plugin/android-nfc/src/main/kotlin/org/eclipse/keyple/plugin/android/nfc/AndroidNfcReader.kt
+++ android/keyple-plugin/android-nfc/src/main/kotlin/org/eclipse/keyple/plugin/android/nfc/AndroidNfcReader.kt
@@ -35,14 +35,21 @@ interface AndroidNfcReader : SmartRemovalReader {
     fun printTagId(): String
 
     /**
-     * Process data from NFC Intent
+     * Process data from NFC Intent. Can be use to handle NFC Tag received when app is
+     * triggered by nfc detection
      *
      * @param intent : Intent received and filterByProtocol by xml tech_list
      */
     fun processIntent(intent: Intent)
 
+    /**
+     * Declare app to handle NFC Tags while in the foreground
+     */
     fun enableNFCReaderMode(activity: Activity)
 
+    /**
+     * Stop app handling NFC Tags while in the foreground
+     */
     fun disableNFCReaderMode(activity: Activity)
 
     companion object {
@@ -50,8 +57,13 @@ interface AndroidNfcReader : SmartRemovalReader {
         val READER_NAME = "AndroidNfcReaderImpl"
         val PLUGIN_NAME = AndroidNfcPlugin.PLUGIN_NAME
 
+        //FLAG_READER_SKIP_NDEF_CHECK Prevent the platform from performing any NDEF checks in reader mode. Must be 0 or 1.
         val FLAG_READER_SKIP_NDEF_CHECK = "FLAG_READER_SKIP_NDEF_CHECK"
+
+        //Allows the caller to prevent the platform from playing sounds when it discovers a tag. Must be 0 or 1.
         val FLAG_READER_NO_PLATFORM_SOUNDS = "FLAG_READER_NO_PLATFORM_SOUNDS"
+
+        //Allows the calling application to specify the delay that the platform will use for performing presence checks on any discovered tag.
         val FLAG_READER_PRESENCE_CHECK_DELAY = "FLAG_READER_PRESENCE_CHECK_DELAY"
     }
 }
