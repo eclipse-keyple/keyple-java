ed2767c848a5aae76f9d494ce31c83b636776e1c
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2020-05-11 20:03:41 +0200

    feat: KEYP-113 update PoSecuritySettings
    - change default kif/kvc/key number management

--- java/component/keyple-calypso/src/main/java/org/eclipse/keyple/calypso/transaction/PoSecuritySettings.java
+++ java/component/keyple-calypso/src/main/java/org/eclipse/keyple/calypso/transaction/PoSecuritySettings.java
@@ -13,7 +13,7 @@ package org.eclipse.keyple.calypso.transaction;
 
 import java.util.EnumMap;
 import java.util.List;
-import org.eclipse.keyple.calypso.transaction.exception.CalypsoUnauthorizedKvcException;
+
 
 /**
  * A class dedicated to managing the security settings involved in managing secure sessions.
@@ -24,70 +24,88 @@ import org.eclipse.keyple.calypso.transaction.exception.CalypsoUnauthorizedKvcEx
  * The getKeyInfo method returns the specified setting value.
  */
 public class PoSecuritySettings {
-    /**
-     * List of key information that can be provided when the secure session is created.
-     */
-    public enum DefaultKeyInfo {
-        /** KIF for personalization used when not provided by the PO */
-        SAM_DEFAULT_KIF_PERSO,
-        /** KIF for load used when not provided by the PO */
-        SAM_DEFAULT_KIF_LOAD,
-        /** KIF for debit used when not provided by the PO */
-        SAM_DEFAULT_KIF_DEBIT,
-        /** Key record number to use when KIF/KVC is unavailable */
-        SAM_DEFAULT_KEY_RECORD_NUMBER
-    }
-
-    /** The default KIF value for personalization */
-    private final static byte DEFAULT_KIF_PERSO = (byte) 0x21;
-    /** The default KIF value for loading */
-    private final static byte DEFAULT_KIF_LOAD = (byte) 0x27;
-    /** The default KIF value for debiting */
-    private final static byte DEFAULT_KIF_DEBIT = (byte) 0x30;
-    /** The default key record number */
-    private final static byte DEFAULT_KEY_RECORD_NUMER = (byte) 0x00;
     /** List of authorized KVCs */
     private List<Byte> authorizedKvcList;
 
-    /** Enummap containing the key information */
-    private final EnumMap<DefaultKeyInfo, Byte> keySettings =
-            new EnumMap<DefaultKeyInfo, Byte>(DefaultKeyInfo.class);
+    /** EnumMap associating session levels and corresponding KIFs */
+    private static EnumMap<PoTransaction.SessionSetting.AccessLevel, Byte> defaultKif =
+            new EnumMap<PoTransaction.SessionSetting.AccessLevel, Byte>(
+                    PoTransaction.SessionSetting.AccessLevel.class);
+    private static EnumMap<PoTransaction.SessionSetting.AccessLevel, Byte> defaultKvc =
+            new EnumMap<PoTransaction.SessionSetting.AccessLevel, Byte>(
+                    PoTransaction.SessionSetting.AccessLevel.class);
+    private static EnumMap<PoTransaction.SessionSetting.AccessLevel, Byte> defaultKeyRecordNumber =
+            new EnumMap<PoTransaction.SessionSetting.AccessLevel, Byte>(
+                    PoTransaction.SessionSetting.AccessLevel.class);
+
+    PoTransaction.SessionSetting.ModificationMode sessionModificationMode =
+            PoTransaction.SessionSetting.ModificationMode.ATOMIC;
+    PoTransaction.SessionSetting.RatificationMode ratificationMode =
+            PoTransaction.SessionSetting.RatificationMode.CLOSE_RATIFIED;
 
     /**
      * Constructor.
-     * <p>
-     * Initialize default values
      */
-    public PoSecuritySettings() {
-        keySettings.put(DefaultKeyInfo.SAM_DEFAULT_KIF_PERSO, DEFAULT_KIF_PERSO);
-        keySettings.put(DefaultKeyInfo.SAM_DEFAULT_KIF_LOAD, DEFAULT_KIF_LOAD);
-        keySettings.put(DefaultKeyInfo.SAM_DEFAULT_KIF_DEBIT, DEFAULT_KIF_DEBIT);
-        keySettings.put(DefaultKeyInfo.SAM_DEFAULT_KEY_RECORD_NUMBER, DEFAULT_KEY_RECORD_NUMER);
+    public PoSecuritySettings() {}
+
+    /**
+     * Set the Session Modification Mode<br>
+     * The default value is ATOMIC
+     * 
+     * @param sessionModificationMode the desired Session Modification Mode
+     * @since 0.9
+     */
+    public void setSessionModificationMode(
+            PoTransaction.SessionSetting.ModificationMode sessionModificationMode) {
+        this.sessionModificationMode = sessionModificationMode;
     }
 
     /**
-     * Associates the specified value with the specified key information in the keySettings map. If
-     * the map previously contained a mapping for this key, the old value is replaced.
+     * Set the Ratification Mode<br>
+     * The default value is CLOSE_RATIFIED
      * 
-     * @param keyInfo - the keyInfo with which the specified value is to be associated
-     * @param value - the value to be associated with the specified key
-     * @return the previous value associated with specified key, or null if there was no mapping for
-     *         key.
+     * @param ratificationMode the desired Ratification Mode
+     * @since 0.9
      */
-    byte putKeyInfo(DefaultKeyInfo keyInfo, Byte value) {
-        return keySettings.put(keyInfo, value);
+    public void setRatificationMode(
+            PoTransaction.SessionSetting.RatificationMode ratificationMode) {
+        this.ratificationMode = ratificationMode;
     }
 
     /**
-     * Returns the value to which the specified key is mapped, or null if this map contains no
-     * mapping for the key.
-     * 
-     * @param keyInfo - the keyInfo whose associated value is to be returned
-     * @return the value to which the specified key is mapped, or null if this map contains no
-     *         mapping for the key
+     * Set the default KIF<br>
+     *
+     * @param sessionAccessLevel the session level
+     * @param kif the desired default KIF
+     * @since 0.9
      */
-    byte getKeyInfo(DefaultKeyInfo keyInfo) {
-        return keySettings.get(keyInfo);
+    public void setSessionDefaultKif(PoTransaction.SessionSetting.AccessLevel sessionAccessLevel,
+            byte kif) {
+        defaultKif.put(sessionAccessLevel, kif);
+    }
+
+    /**
+     * Set the default KVC<br>
+     *
+     * @param sessionAccessLevel the session level
+     * @param kvc the desired default KVC
+     * @since 0.9
+     */
+    public void setSessionDefaultKvc(PoTransaction.SessionSetting.AccessLevel sessionAccessLevel,
+            byte kvc) {
+        defaultKvc.put(sessionAccessLevel, kvc);
+    }
+
+    /**
+     * Set the default key record number<br>
+     *
+     * @param sessionAccessLevel the session level
+     * @param keyRecordNumber the desired default key record number
+     * @since 0.9
+     */
+    public void setSessionDefaultKeyRecordNumber(
+            PoTransaction.SessionSetting.AccessLevel sessionAccessLevel, byte keyRecordNumber) {
+        defaultKeyRecordNumber.put(sessionAccessLevel, keyRecordNumber);
     }
 
     /**
@@ -95,24 +113,73 @@ public class PoSecuritySettings {
      *
      * If this method is not called, the list will remain empty and all KVCs will be accepted.
      *
-     * If a list is provided and a PO with a KVC not belonging to this list is presented, a
-     * {@link CalypsoUnauthorizedKvcException} will be raised.
-     *
      * @param authorizedKvcList the list of authorized KVCs
      */
-    public void setAuthorizedKvcList(List<Byte> authorizedKvcList) {
+    public void setSessionAuthorizedKvcList(List<Byte> authorizedKvcList) {
         this.authorizedKvcList = authorizedKvcList;
     }
 
     /**
-     * CHeck if the provided kvc value is authorized or not.
+     * (package-private)<br>
+     * 
+     * @return the Session Modification Mode
+     * @since 0.9
+     */
+    PoTransaction.SessionSetting.ModificationMode getSessionModificationMode() {
+        return sessionModificationMode;
+    }
+
+    /**
+     * (package-private)<br>
+     * 
+     * @return the Ratification Mode
+     * @since 0.9
+     */
+    PoTransaction.SessionSetting.RatificationMode getRatificationMode() {
+        return ratificationMode;
+    }
+
+    /**
+     * (package-private)<br>
+     * 
+     * @return the default session KIF
+     * @since 0.9
+     */
+    Byte getSessionDefaultKif(PoTransaction.SessionSetting.AccessLevel sessionAccessLevel) {
+        return defaultKif.get(sessionAccessLevel);
+    }
+
+    /**
+     * (package-private)<br>
+     * 
+     * @return the default session KVC
+     * @since 0.9
+     */
+    Byte getSessionDefaultKvc(PoTransaction.SessionSetting.AccessLevel sessionAccessLevel) {
+        return defaultKvc.get(sessionAccessLevel);
+    }
+
+    /**
+     * (package-private)<br>
+     *
+     * @return the default session key record number
+     * @since 0.9
+     */
+    Byte getSessionDefaultKeyRecordNumber(
+            PoTransaction.SessionSetting.AccessLevel sessionAccessLevel) {
+        return defaultKeyRecordNumber.get(sessionAccessLevel);
+    }
+
+    /**
+     * (package-private)<br>
+     * Check if the provided kvc value is authorized or not.
      * <p>
      * If no list of authorized kvc is defined (authorizedKvcList null), all kvc are authorized.
-     * 
+     *
      * @param kvc to be tested
      * @return true if the kvc is authorized
      */
-    public boolean isAuthorizedKvc(byte kvc) {
+    public boolean isSessionKvcAuthorized(byte kvc) {
         return authorizedKvcList == null || authorizedKvcList.contains(kvc);
     }
 }
