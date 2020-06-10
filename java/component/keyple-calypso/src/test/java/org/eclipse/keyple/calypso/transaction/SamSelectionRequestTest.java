1ef53948717aab71d19d83c77942e4c77880352a
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2020-06-10 19:42:02 +0200

    feat: KEYP-12/102-139 update CalypsoSam SamSelectionRequest unit tests

--- java/component/keyple-calypso/src/test/java/org/eclipse/keyple/calypso/transaction/SamSelectionRequestTest.java
+++ java/component/keyple-calypso/src/test/java/org/eclipse/keyple/calypso/transaction/SamSelectionRequestTest.java
@@ -11,33 +11,32 @@
  ********************************************************************************/
 package org.eclipse.keyple.calypso.transaction;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import org.eclipse.keyple.calypso.command.sam.SamRevision;
+import org.eclipse.keyple.core.seproxy.message.AnswerToReset;
+import org.eclipse.keyple.core.seproxy.message.SeResponse;
+import org.eclipse.keyple.core.seproxy.message.SelectionStatus;
+import org.eclipse.keyple.core.seproxy.protocol.SeCommonProtocols;
+import org.eclipse.keyple.core.util.ByteArrayUtil;
 import org.junit.Test;
 
 public class SamSelectionRequestTest {
+    private static final String REVISION = "C1";
+    private static final String SN = "11223344";
+    private static final String ATR = "3B3F9600805A4880" + REVISION + "205017" + SN + "829000";
 
     @Test
-    public void parse() {
-        // TODO rework this unit test
-        // SamSelector samSelector = new SamSelector(AUTO, null, "Dummy SeSelector");
-        // SamSelectionRequest samSelectionRequest = new SamSelectionRequest(samSelector);
-        // SelectionStatus selectionStatus = new SelectionStatus(
-        // new AnswerToReset(ByteArrayUtil.fromHex("3B001122805A0180D002030411223344829000")),
-        // null, true);
-        // CalypsoSam calypsoSam =
-        // samSelectionRequest.parse(new SeResponse(true, true, selectionStatus, null));
-        // assertEquals(calypsoSam.getSelectionExtraInfo(), "Dummy SeSelector");
-    }
-
-    @Test // (expected = IllegalStateException.class)
-    public void getCommandParser() {
-        // TODO rework this unit test
-        // SamSelector samSelector = new SamSelector(AUTO, null, "Dummy SeSelector");
-        // SamSelectionRequest samSelectionRequest = new SamSelectionRequest(samSelector);
-        // SelectionStatus selectionStatus = new SelectionStatus(
-        // new AnswerToReset(ByteArrayUtil.fromHex("3B001122805A0180D002030411223344829000")),
-        // null, true);
-        // samSelectionRequest.getCommandParser(new SeResponse(true, true, selectionStatus, null),
-        // 0);
+    public void samSelectionRequest_parse() {
+        SamSelector samSelector =
+                SamSelector.builder().seProtocol(SeCommonProtocols.PROTOCOL_ISO7816_3)
+                        .samRevision(SamRevision.AUTO).build();
+        SamSelectionRequest samSelectionRequest = new SamSelectionRequest(samSelector);
+        SelectionStatus selectionStatus =
+                new SelectionStatus(new AnswerToReset(ByteArrayUtil.fromHex(ATR)), null, true);
+        CalypsoSam calypsoSam =
+                samSelectionRequest.parse(new SeResponse(true, true, selectionStatus, null));
+        // minimal checks on the CalypsoSam result
+        assertThat(calypsoSam.getSamRevision()).isEqualTo(SamRevision.C1);
+        assertThat(calypsoSam.getSerialNumber()).isEqualTo(ByteArrayUtil.fromHex(SN));
     }
 }
