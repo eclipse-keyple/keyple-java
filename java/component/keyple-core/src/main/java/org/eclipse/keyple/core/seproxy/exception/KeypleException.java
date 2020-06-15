c560ad9ea1cfb96e8f31c635e3693a8fdf7d30b6
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2020-06-15 10:36:37 +0200

    feat: KEYP-12/295 make KeypleException extend RuntimeException

--- java/component/keyple-core/src/main/java/org/eclipse/keyple/core/seproxy/exception/KeypleException.java
+++ java/component/keyple-core/src/main/java/org/eclipse/keyple/core/seproxy/exception/KeypleException.java
@@ -15,7 +15,7 @@ package org.eclipse.keyple.core.seproxy.exception;
  * The exception {@code KeypleException} is the parent abstract class of all Keyple checked
  * exceptions.
  */
-public abstract class KeypleException extends Exception {
+public abstract class KeypleException extends RuntimeException {
 
     /**
      * @param message the message to identify the exception context
