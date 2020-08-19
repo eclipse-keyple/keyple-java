2e0e89cba7c643f459c0e81b6cc0521600a69bd3
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2020-08-19 18:33:07 +0200

    refact: KEYP-319 fix Javadoc

--- java/component/keyple-core/src/main/java/org/eclipse/keyple/core/seproxy/event/ObservableReader.java
+++ java/component/keyple-core/src/main/java/org/eclipse/keyple/core/seproxy/event/ObservableReader.java
@@ -179,7 +179,7 @@ public interface ObservableReader extends SeReader {
             NotificationMode notificationMode, PollingMode pollingMode);
 
     /**
-     * Termine le traitement du SE, en particulier <br>
+     * Terminates the processing of the SE, in particular after an interruption by exception<br>
      * Do nothing if the channel is already closed.<br>
      * Channel closing is nominally managed by using the CLOSE_AFTER flag during the last
      * transmission with the SE. However, there are cases where exchanges with the SE are
