0654db0e7cd5a58e30968fb9b7dd36a1e8ab1f50
Andrei Cristea <andrei.cristea019@gmail.com>
2020-08-28 11:32:01 +0200

    refactor: fix merge from develop

--- java/component/keyple-plugin/remote-se/virtualse/src/main/java/org/eclipse/keyple/plugin/remotese/virtualse/impl/AbstractVirtualReader.java
+++ java/component/keyple-plugin/remote-se/virtualse/src/main/java/org/eclipse/keyple/plugin/remotese/virtualse/impl/AbstractVirtualReader.java
@@ -18,7 +18,7 @@ import org.eclipse.keyple.core.seproxy.MultiSeRequestProcessing;
 import org.eclipse.keyple.core.seproxy.message.ChannelControl;
 import org.eclipse.keyple.core.seproxy.message.SeRequest;
 import org.eclipse.keyple.core.seproxy.message.SeResponse;
-import org.eclipse.keyple.core.seproxy.plugin.AbstractReader;
+import org.eclipse.keyple.core.seproxy.plugin.reader.AbstractReader;
 import org.eclipse.keyple.core.seproxy.protocol.SeProtocol;
 import org.eclipse.keyple.core.seproxy.protocol.TransmissionMode;
 import org.eclipse.keyple.core.util.json.BodyError;
