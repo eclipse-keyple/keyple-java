7c5b596e507ae2ad3bf31da898a753d96ddc27a5
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2020-10-08 08:45:59 +0200

    feat: KEYP-336 spotless

--- java/component/keyple-plugin/pcsc/src/main/java/org/eclipse/keyple/plugin/pcsc/PcscProtocolSetting.java
+++ java/component/keyple-plugin/pcsc/src/main/java/org/eclipse/keyple/plugin/pcsc/PcscProtocolSetting.java
@@ -50,9 +50,7 @@ final class PcscProtocolSetting {
 
     map.put(PcscSupportedProtocols.MIFARE_DESFIRE.name(), "3B8180018080");
 
-    map.put(
-        PcscSupportedProtocols.MEMORY_ST25.name(),
-        "3B8F8001804F0CA000000306070007D0020C00B6");
+    map.put(PcscSupportedProtocols.MEMORY_ST25.name(), "3B8F8001804F0CA000000306070007D0020C00B6");
 
     map.put(PcscSupportedProtocols.ISO_7816_3.name(), "3.*");
 
