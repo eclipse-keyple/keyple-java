95abf924f366f2e3bb931e851e774f5cb0534a8f
OlivierDelcroix <odelcroi@gmail.com>
2020-10-23 14:43:28 +0200

    feat/keyp-276 fix UT
    Signed-off-by: OlivierDelcroix <odelcroi@gmail.com>

--- java/component/keyple-plugin/remote/native/src/test/java/org/eclipse/keyple/plugin/remote/nativ/impl/NativePoolServerServiceTest.java
+++ java/component/keyple-plugin/remote/native/src/test/java/org/eclipse/keyple/plugin/remote/nativ/impl/NativePoolServerServiceTest.java
@@ -170,7 +170,6 @@ public class NativePoolServerServiceTest extends BaseNativeTest {
 
   @Test
   public void onIsPresent_shouldPropagate_toLocalPoolPlugin() {
-    doReturn(true).when(readerMocked).isSePresent();
     KeypleMessageDto request = getIsSePresentDto(sessionId);
     NativePoolServerUtils.getAsyncNode().onMessage(request);
 
