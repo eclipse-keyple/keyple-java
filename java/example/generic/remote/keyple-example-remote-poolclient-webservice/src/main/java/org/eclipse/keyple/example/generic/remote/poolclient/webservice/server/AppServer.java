0469ce7fd47f5df1d0d40b6354def7ca9e1c8722
OlivierDelcroix <odelcroi@gmail.com>
2020-11-30 10:49:59 +0100

    feat/keyp-41 move example
    Signed-off-by: OlivierDelcroix <odelcroi@gmail.com>

---
+++ java/example/generic/remote/keyple-example-remote-poolclient-webservice/src/main/java/org/eclipse/keyple/example/generic/remote/poolclient/webservice/server/AppServer.java
@@ -0,0 +1,85 @@
+/* **************************************************************************************
+ * Copyright (c) 2020 Calypso Networks Association https://www.calypsonet-asso.org/
+ *
+ * See the NOTICE file(s) distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License 2.0 which is available at http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ ************************************************************************************** */
+package org.eclipse.keyple.example.generic.remote.poolclient.webservice.server;
+
+import javax.enterprise.context.ApplicationScoped;
+import org.eclipse.keyple.core.service.PoolPlugin;
+import org.eclipse.keyple.core.service.Reader;
+import org.eclipse.keyple.core.service.SmartCardService;
+import org.eclipse.keyple.core.service.util.ContactlessCardCommonProtocols;
+import org.eclipse.keyple.example.generic.remote.poolclient.webservice.util.CalypsoSmartCard;
+import org.eclipse.keyple.plugin.remote.LocalServiceClient;
+import org.eclipse.keyple.plugin.remote.impl.PoolLocalServiceServerFactory;
+import org.eclipse.keyple.plugin.stub.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/** Example of a server side application. */
+@ApplicationScoped
+public class AppServer {
+
+  private static final Logger logger = LoggerFactory.getLogger(AppServer.class);
+
+  private PoolPlugin poolPlugin;
+
+  /**
+   * Initialize the server components :
+   *
+   * <ul>
+   *   <li>A {@link PoolPlugin} with a {@link StubPoolPlugin} with a inserted card,
+   *   <li>A {@link LocalServiceClient} with a sync node bind to a {@link
+   *       org.eclipse.keyple.plugin.remote.spi.SyncEndpointClient} endpoint.
+   * </ul>
+   */
+  public void init() {
+
+    // Init a local pool plugin.
+    initStubPoolPlugin();
+
+    // Init the local service using the associated factory.
+    PoolLocalServiceServerFactory.builder()
+        .withSyncNode() // HTTP webservice needs a server sync node configuration
+        .withPoolPlugins(poolPlugin.getName()) // use the registered ReaderPoolPlugin
+        .getService();
+  }
+
+  /** Init a local pool plugin with a stub pool plugin and an inserted card */
+  private void initStubPoolPlugin() {
+
+    String STUB_PLUGIN_NAME = "stubPoolPlugin";
+    String STUB_READER_NAME = "stubReader";
+    String REFERENCE_GROUP = "group1";
+
+    // Registers the plugin to the smart card service.
+    poolPlugin =
+        (StubPoolPlugin)
+            SmartCardService.getInstance()
+                .registerPlugin(new StubPoolPluginFactory(STUB_PLUGIN_NAME, null, null));
+
+    // Plug manually to the plugin a local reader associated in a group reference.
+    ((StubPoolPlugin) poolPlugin)
+        .plugPoolReader(REFERENCE_GROUP, STUB_READER_NAME, new CalypsoSmartCard());
+
+    // Retrieves the connected reader from the plugin.
+    Reader nativeReader = poolPlugin.getReader(STUB_READER_NAME);
+
+    // Activates the protocol ISO_14443_4 on the reader.
+    nativeReader.activateProtocol(
+        StubSupportedProtocols.ISO_14443_4.name(),
+        ContactlessCardCommonProtocols.ISO_14443_4.name());
+
+    logger.info(
+        "Server - Local reader was configured with a STUB reader : {} in group reference : {}",
+        nativeReader.getName(),
+        REFERENCE_GROUP);
+  }
+}
