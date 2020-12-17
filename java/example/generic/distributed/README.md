eb142b060822460c52f69e8ce32a68057ac77981
andrei-cristea <andrei.cristea019@gmail.com>
2020-12-17 11:22:37 +0100

    refactor(Distributed): change examples names and description (#494)
    * refactor(Distributed): change examples names and description
    
    * Wrong permissions for gradlew
    
    * refactor(Distributed): change examples names and description
    
    Co-authored-by: Brice Ruppen <brice.ruppen@armotic.fr>

--- java/example/generic/distributed/README.md
+++ java/example/generic/distributed/README.md
@@ -1,16 +1,19 @@
-Getting Started - Remote Example
+Getting Started - Keyple Distributed Examples
 ---
 
-Those projects demonstrate the multiple use cases covered by the Remote Plugin. 
+Those projects demonstrate the multiple use cases covered by the **Keyple Distributed** solution. 
 
-# Use Case #1 : Remote Plugin Server with Web Service example
-This project showcases the synchronous mode of the Remote Plugin Server with a Web Service protocol.
-This plugin allows a client application to control a smart card reader available on a server (e.g. SAM reader) see the [developer guide](https://calypsonet.github.io/keyple-website/docs/developer-guide/distributed-application/#remotepluginserver).
+# Use Case #1 : Reader Server Side with Sync Network Protocol (Web Service) example
+This project showcases the synchronous mode of the **Keyple Distributed** solution with a Web Service protocol.
+This configuration allows a **server** application to control a smart card reader available on a **client** (e.g. PO reader).
+See the [developer guide](https://keyple.org/docs/developer-guide/distributed-application/#reader-client-side).
 
-# Use Case #1 : Remote Plugin Server with Web Socket example
-This project showcases the implementation of the asynchronous mode of the Remote Plugin Server with a Web Socket protocol.
-This plugin allows a client application to control a smart card reader available on a server (e.g. SAM reader), see the [developer guide](https://calypsonet.github.io/keyple-website/docs/developer-guide/distributed-application/#remotepluginserver).
+# Use Case #1 : Reader Server Side with Async Network Protocol (Web Socket) example
+This project showcases the implementation of the asynchronous mode of the **Keyple Distributed** solution with a Web Socket protocol.
+This configuration allows a **server** application to control a smart card reader available on a **client** (e.g. PO reader).
+See the [developer guide](https://keyple.org/docs/developer-guide/distributed-application/#reader-client-side).
 
-# Use Case #7 : Pool Remote Plugin Client with Web Service example
-This project showcases the synchronous mode of the Pool Remote Plugin Client with a Web Service.
-This plugin allows a client application to control a pool of smart card readers available on a server (e.g. HSM readers); see the [developer guide](https://calypsonet.github.io/keyple-website/docs/developer-guide/distributed-application/#poolremotepluginclient) for more details.
+# Use Case #7 : Pool Reader Server Side with Web Sync Network Protocol (Web Service) example
+This project showcases the synchronous mode of the **Keyple Distributed** solution with a Web Service.
+This configuration allows a **client** application to control a **pool** of smart cards readers available on a **server** (e.g. HSM readers).
+See the [developer guide](https://keyple.org/docs/developer-guide/distributed-application/#pool-reader-server-side) for more details.
---
+++ java/example/generic/distributed/UseCase7_PoolReaderServerSide_Webservice/src/main/java/org/eclipse/keyple/example/generic/distributed/poolclient/webservice/util/CalypsoTicketingService.java
@@ -0,0 +1,110 @@
+/* **************************************************************************************
+ * Copyright (c) 2020 Calypso Networks Association https://www.calypsonet-asso.org/
+ *
+ * See the NOTICE file(s) distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License 2.0 which is available at http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ ************************************************************************************** */
+package org.eclipse.keyple.example.generic.distributed.poolclient.webservice.util;
+
+import org.eclipse.keyple.calypso.transaction.*;
+import org.eclipse.keyple.core.card.selection.CardResource;
+import org.eclipse.keyple.core.card.selection.CardSelectionsService;
+import org.eclipse.keyple.core.card.selection.CardSelector;
+import org.eclipse.keyple.core.service.Reader;
+import org.eclipse.keyple.core.service.util.ContactlessCardCommonProtocols;
+import org.eclipse.keyple.core.util.ByteArrayUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/** Contains Calypso Ticketing Logic utilities. */
+public class CalypsoTicketingService {
+
+  private static final Logger logger = LoggerFactory.getLogger(CalypsoTicketingService.class);
+
+  /** AID: Keyple */
+  public static final String AID = "315449432E49434131";
+
+  public static final byte RECORD_NUMBER_1 = 1;
+  public static final byte SFI_EnvironmentAndHolder = (byte) 0x07;
+  public static final byte SFI_EventLog = (byte) 0x08;
+
+  /**
+   * Prepare a Selection object ready to select Calypso card and read environment file
+   *
+   * @return instance of Selection object
+   */
+  public static CardSelectionsService getCardSelection() {
+
+    // Prepare PO Selection
+    CardSelectionsService cardSelection = new CardSelectionsService();
+
+    // Calypso selection
+    PoSelection poSelectionRequest =
+        new PoSelection(
+            PoSelector.builder()
+                .cardProtocol(ContactlessCardCommonProtocols.ISO_14443_4.name())
+                .aidSelector(CardSelector.AidSelector.builder().aidToSelect(AID).build())
+                .invalidatedPo(PoSelector.InvalidatedPo.REJECT)
+                .build());
+
+    // Prepare the reading order.
+    poSelectionRequest.prepareReadRecordFile(SFI_EnvironmentAndHolder, RECORD_NUMBER_1);
+
+    // Add the selection case to the current selection
+    cardSelection.prepareSelection(poSelectionRequest);
+    return cardSelection;
+  }
+
+  /**
+   * Reads and returns content of event log file within a Portable Object Transaction.
+   *
+   * @param calypsoPo smartcard to read to the event log file
+   * @param reader local reader where the smartcard is inserted
+   * @return content of the event log file in Hexadecimal
+   */
+  public static String readEventLog(CalypsoPo calypsoPo, Reader reader) {
+
+    // Execute calypso session from a card selection
+    logger.info(
+        "Initial PO Content, atr : {}, sn : {}",
+        calypsoPo.getAtr(),
+        calypsoPo.getApplicationSerialNumber());
+
+    // Retrieves the data read from the CalypsoPo updated during the transaction process.
+    ElementaryFile efEnvironmentAndHolder = calypsoPo.getFileBySfi(SFI_EnvironmentAndHolder);
+    String environmentAndHolder =
+        ByteArrayUtil.toHex(efEnvironmentAndHolder.getData().getContent());
+
+    // Logs the result.
+    logger.info("EnvironmentAndHolder file data: {}", environmentAndHolder);
+
+    // Go on with the reading of the first record of the EventLog file.
+    logger.info("= #### reading transaction of the EventLog file.");
+
+    PoTransaction poTransaction = new PoTransaction(new CardResource<>(reader, calypsoPo));
+
+    // Prepares the reading order and keep the associated parser for later use once the
+    // transaction has been processed.
+    poTransaction.prepareReadRecordFile(SFI_EventLog, RECORD_NUMBER_1);
+
+    // Actual PO communication: send the prepared read order, then close the channel with
+    // the PO.
+    poTransaction.prepareReleasePoChannel();
+    poTransaction.processPoCommands();
+    logger.info("The reading of the EventLog has succeeded.");
+
+    // Retrieves the data read from the CalypsoPo updated during the transaction process.
+    ElementaryFile efEventLog = calypsoPo.getFileBySfi(SFI_EventLog);
+    String eventLog = ByteArrayUtil.toHex(efEventLog.getData().getContent());
+
+    // Logs the result.
+    logger.info("EventLog file data: {}", eventLog);
+
+    return eventLog;
+  }
+}
